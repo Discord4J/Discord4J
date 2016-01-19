7a771457975c2651078ce9f5713bccf8d8084f85
austinv11 <austinv11@gmail.com>
2016-01-18 19:49:18 -0500

    Added custom implementations of IChannel methods for Private and Voice channels

--- src/main/java/sx/blah/discord/handle/impl/obj/PrivateChannel.java
+++ src/main/java/sx/blah/discord/handle/impl/obj/PrivateChannel.java
@@ -19,14 +19,12 @@
 
 package sx.blah.discord.handle.impl.obj;
 
+import sx.blah.discord.api.DiscordException;
 import sx.blah.discord.api.IDiscordClient;
-import sx.blah.discord.handle.obj.IMessage;
-import sx.blah.discord.handle.obj.IPrivateChannel;
-import sx.blah.discord.handle.obj.IUser;
+import sx.blah.discord.handle.obj.*;
+import sx.blah.discord.util.HTTP403Exception;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class PrivateChannel extends Channel implements IPrivateChannel {
 	
@@ -43,6 +41,111 @@ public class PrivateChannel extends Channel implements IPrivateChannel {
 		super(client, recipient.getName(), id, null, null, 0, messages, new HashMap<>(), new HashMap<>());
 		this.recipient = recipient;
 		this.isPrivate = true;
+	}
+	
+	@Override
+	public Map<String, PermissionOverride> getUserOverrides() {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public Map<String, PermissionOverride> getRoleOverrides() {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public EnumSet<Permissions> getModifiedPermissions(IUser user) {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public EnumSet<Permissions> getModifiedPermissions(IRole role) {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public void addUserOverride(String userId, PermissionOverride override) {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public void addRoleOverride(String roleId, PermissionOverride override) {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public void removePermissionsOverride(String id) throws HTTP403Exception {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public void overrideRolePermissions(String roleID, EnumSet<Permissions> toAdd, EnumSet<Permissions> toRemove) throws HTTP403Exception {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public void overrideUserPermissions(String userID, EnumSet<Permissions> toAdd, EnumSet<Permissions> toRemove) throws HTTP403Exception {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public void delete() throws HTTP403Exception {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public void setPosition(int position) {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public int getPosition() {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public void edit(Optional<String> name, Optional<Integer> position, Optional<String> topic) throws DiscordException, HTTP403Exception {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public String mention() {
+		return recipient.mention();
+	}
+	
+	@Override
+	public void setTopic(String topic) {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public String getTopic() {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public IGuild getGuild() {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public IGuild getParent() {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public String getID() {
+		return recipient.getID();
+	}
+	
+	@Override
+	public String getName() {
+		return recipient.getName();
+	}
+	
+	@Override
+	public void setName(String name) {
+		throw new UnsupportedOperationException();
 	}
 	
 	@Override
