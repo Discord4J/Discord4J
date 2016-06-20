91611c9c01d508e8c3d33795733b385a2b27d02c
grandmind <grandmind@users.noreply.github.com>
2016-06-20 11:11:10 -0500

    Updated examples (#52)
    * Updated examples
    
    * Updated comment to reflect changes.
    
    * Comments have spaces >.>

--- src/test/java/sx/blah/discord/examples/ReadyBot.java
+++ src/test/java/sx/blah/discord/examples/ReadyBot.java
@@ -1,22 +1,22 @@
 package sx.blah.discord.examples;
 
-import sx.blah.discord.util.DiscordException;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.events.IListener;
 import sx.blah.discord.handle.impl.events.ReadyEvent;
+import sx.blah.discord.util.DiscordException;
 import sx.blah.discord.util.RateLimitException;
 
 /**
- * This is a simple example to demonstrate the need to wait for the ReadyEvent before attempting to do things in Discord.
+ * An example demonstrating the need to wait for the ReadyEvent before attempting to do things in Discord.
  */
 public class ReadyBot extends BaseBot implements IListener<ReadyEvent> {
 
 	public ReadyBot(IDiscordClient discordClient) {
 		super(discordClient);
-		discordClient.getDispatcher().registerListener(this); // Registers this class as an event listener
+		discordClient.getDispatcher().registerListener(this); // Registers this bot as an event listener
 
 		try {
-			// This will NOT work. The bot is not ready to interact with Discord because the ReadyEvent has not yet been fired.
+			// This will NOT work. The bot is not ready to interact with Discord because the ReadyEvent has yet to be fired.
 			discordClient.changeUsername("Loser Bot");
 		} catch (RateLimitException | DiscordException e) {
 			e.printStackTrace();
@@ -26,8 +26,6 @@ public class ReadyBot extends BaseBot implements IListener<ReadyEvent> {
 	/**
 	 * The ReadyEvent is fired when the bot is ready to interact with Discord. Attempting to do so (e.g. Changing account info, sending messages, etc.)
 	 * before the event is fired will result in an error.
-	 *
-	 * @param event The event object.
 	 */
 	@Override
 	public void handle(ReadyEvent event) {
