3b4a81f3758734b281b684b7733512cdf56a06c8
austinv11 <austinv11@gmail.com>
2016-07-19 15:47:59 -0400

    Extracted AudioEncodingType to its own class

---
+++ src/main/java/sx/blah/discord/handle/audio/AudioEncodingType.java
@@ -0,0 +1,15 @@
+package sx.blah.discord.handle.audio;
+
+/**
+ * The types of specific audio handling Discord4J supports.
+ */
+public enum AudioEncodingType {
+	/**
+	 * When the audio encoding type is PCM, the audio will automatically be converted to opus.
+	 */
+	PCM,
+	/**
+	 * When the audio encoding type is opus, the audio is sent directly to discord as is.
+	 */
+	OPUS
+}
