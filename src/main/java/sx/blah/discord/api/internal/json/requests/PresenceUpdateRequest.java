21a7cff478c0ea8a89b22ec4b23ae74ec30389ce
grandmind <darichey1@gmail.com>
2016-10-06 17:12:02 -0500

    Fixed NPE on status change

--- src/main/java/sx/blah/discord/api/internal/json/requests/PresenceUpdateRequest.java
+++ src/main/java/sx/blah/discord/api/internal/json/requests/PresenceUpdateRequest.java
@@ -7,14 +7,14 @@ public class PresenceUpdateRequest {
 	/**
 	 * The time (in epoch milliseconds) since the user became idle or null if not idle
 	 */
-	public long idle_since;
+	public Long idle_since; // This must be the boxed Long because it can be null
 
 	/**
 	 * The game the user is playing, or null if no game
 	 */
 	public GameObject game;
 
-	public PresenceUpdateRequest(long idle_since, Status status) {
+	public PresenceUpdateRequest(Long idle_since, Status status) {
 		this.idle_since = idle_since;
 		this.game = new GameObject(status);
 	}
