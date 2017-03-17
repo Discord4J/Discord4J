5b8409f1099ab8102097910e96b65183fcad5498
grandmind <darichey1@gmail.com>
2017-03-17 17:10:12 -0500

    Document IAudioReceiver

--- src/main/java/sx/blah/discord/handle/audio/IAudioReceiver.java
+++ src/main/java/sx/blah/discord/handle/audio/IAudioReceiver.java
@@ -2,9 +2,25 @@ package sx.blah.discord.handle.audio;
 
 import sx.blah.discord.handle.obj.IUser;
 
+/**
+ * Handles the receiving of audio from Discord.
+ */
 public interface IAudioReceiver {
+
+	/**
+	 * Called when audio is received.
+	 *
+	 * @param audio The packet of audio. Format depends on {{@link #getAudioEncodingType()}}
+	 * @param user The user this audio was received from.
+	 */
 	void receive(byte[] audio, IUser user);
 
+	/**
+	 * This is called to determine the type of audio data provided by this receiver. This determines how the audio data
+	 * is processed.
+	 *
+	 * @return The audio encoding type. By default this returns {@link AudioEncodingType#PCM}.
+	 */
 	default AudioEncodingType getAudioEncodingType() {
 		return AudioEncodingType.PCM;
 	}
