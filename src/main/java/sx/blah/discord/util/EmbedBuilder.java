9ac185a073887840a0ad8e85f9ad17c183fa45d4
xaanit <shadowjacob@yahoo.com>
2017-03-22 23:09:59 -0500

    Added an additional appendField
    Allows the copying of a field

--- src/main/java/sx/blah/discord/util/EmbedBuilder.java
+++ src/main/java/sx/blah/discord/util/EmbedBuilder.java
@@ -416,6 +416,22 @@ public class EmbedBuilder {
 	}
 
 	/**
+	 * Copys the information of an IEmbedField.
+	 *
+	 * @param field   The field to copy
+	 * @return Itself for chaining
+	 * @see #setLenient(boolean)
+	 */
+	public EmbedBuilder appendField(IEmbedField field) {
+		if (field == null)
+			if (lenient)
+				return this;
+			else
+				throw new IllegalArgumentException("Field can not be null!");
+		return appendField(field.getName(), field.getValue(), field.isInline());
+	}
+
+	/**
 	 * Returns the number of fields in the builder.
 	 *
 	 * @return The number of fields in the builder.
