202dcceb406affb74f0897ce853db5ee9a254ee2
crafty <oopsjpeg@gmail.com>
2017-03-30 21:00:04 -0400

    Add getUsersByName method to client (#242)

--- src/main/java/sx/blah/discord/api/IDiscordClient.java
+++ src/main/java/sx/blah/discord/api/IDiscordClient.java
@@ -268,6 +268,23 @@ public interface IDiscordClient {
 	IUser getUserByID(String userID);
 
 	/**
+	 * Gets a set of users by their name, case-sensitive.
+	 *
+	 * @param name The name of the desired user(s).
+	 * @return A {@link Collection} of {@link User} objects with the provided name.
+	 */
+	List<IUser> getUsersByName(String name);
+
+	/**
+	 * Gets a set of users by their name.
+	 *
+	 * @param name The name of the desired user(s).
+	 * @param ignoreCase Whether to ignore the case of the user's name.
+	 * @return A {@link Collection} of {@link User} objects with the provided name.
+	 */
+	List<IUser> getUsersByName(String name, boolean ignoreCase);
+
+	/**
 	 * Gets a set of all roles visible to the bot user.
 	 *
 	 * @return A {@link Collection} of all {@link Role} objects.
