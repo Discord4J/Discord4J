bca2f50edb9f6499a0c6b5433095e15663ac7b5b
grandmind <darichey1@gmail.com>
2017-07-16 20:02:16 -0500

    Fix reaction adds for cached messages.

--- src/main/java/sx/blah/discord/api/internal/DispatchHandler.java
+++ src/main/java/sx/blah/discord/api/internal/DispatchHandler.java
@@ -735,6 +735,13 @@ class DispatchHandler {
 		IReaction reaction = event.emoji.id == null
 				? message.getReactionByUnicode(event.emoji.name)
 				: message.getReactionByID(Long.parseUnsignedLong(event.emoji.id));
+
+		if (reaction == null) { // Only happens in the case of a cached message with a new reaction
+			long id = event.emoji.id == null ? 0 : Long.parseUnsignedLong(event.emoji.id);
+			reaction = new Reaction(message, 1, ReactionEmoji.of(event.emoji.name, id));
+			message.getReactions().add(reaction);
+		}
+
 		IUser user = channel.getGuild().getUserByID(Long.parseUnsignedLong(event.user_id));
 
 		client.dispatcher.dispatch(new ReactionAddEvent(message, reaction, user));
@@ -749,10 +756,12 @@ class DispatchHandler {
 		IReaction reaction = event.emoji.id == null
 				? message.getReactionByUnicode(event.emoji.name)
 				: message.getReactionByID(Long.parseUnsignedLong(event.emoji.id));
+
 		if (reaction == null) { // the last reaction of the emoji was removed
 			long id = event.emoji.id == null ? 0 : Long.parseUnsignedLong(event.emoji.id);
 			reaction = new Reaction(message, 0, ReactionEmoji.of(event.emoji.name, id));
 		}
+
 		IUser user = channel.getGuild().getUserByID(Long.parseUnsignedLong(event.user_id));
 
 		client.dispatcher.dispatch(new ReactionRemoveEvent(message, reaction, user));
