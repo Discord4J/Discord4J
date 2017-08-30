40458a065a3be8c2cd53940c2c9ed2c973d8cf6b
GrandPanda <darichey1@gmail.com>
2017-08-30 17:34:44 -0500

    Refactor request stream consumer

--- rest/src/main/java/discord4j/rest/request/RequestStream.java
+++ rest/src/main/java/discord4j/rest/request/RequestStream.java
@@ -1,17 +1,76 @@
 package discord4j.rest.request;
 
+import discord4j.rest.http.client.ExchangeFilter;
+import discord4j.rest.http.client.SimpleHttpClient;
+import io.netty.handler.codec.http.HttpHeaders;
 import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.function.Consumer;
 
 public class RequestStream<T> {
 
 	private final EmitterProcessor<DiscordRequest<T>> backing = EmitterProcessor.create(false);
+	private final SimpleHttpClient httpClient;
+
+	RequestStream(SimpleHttpClient httpClient) {
+		this.httpClient = httpClient;
+	}
 
 	void push(DiscordRequest<T> request) {
 		backing.onNext(request);
 	}
 
-	Mono<DiscordRequest<T>> read() {
+	void start() {
+		read().subscribe(new Reader());
+	}
+
+	private Mono<DiscordRequest<T>> read() {
 		return backing.next();
+	}
+
+	private class Reader implements Consumer<DiscordRequest<T>> {
+
+		private volatile Duration sleepTime = Duration.ZERO;
+		private final ExchangeFilter exchangeFilter = ExchangeFilter.builder()
+				.responseFilter(response -> {
+					HttpHeaders headers = response.responseHeaders();
+
+					int remaining = headers.getInt("X-RateLimit-Remaining");
+					if (remaining == 0) {
+						long resetAt = Long.parseLong(headers.get("X-RateLimit-Reset"));
+						long discordTime = OffsetDateTime.parse(headers.get("Date"),
+								DateTimeFormatter.RFC_1123_DATE_TIME).toEpochSecond();
+
+						sleepTime = Duration.ofSeconds(resetAt - discordTime);
+					}
+				})
+				.build();
+
+		@SuppressWarnings("ConstantConditions")
+		@Override
+		public void accept(DiscordRequest<T> req) {
+			httpClient.exchange(req.getMethod(), req.getUri(), req.getBody(), req.getResponseType(), exchangeFilter)
+					.materialize()
+					.subscribe(signal -> {
+						if (signal.isOnSubscribe()) {
+							req.mono.onSubscribe(signal.getSubscription());
+						} else if (signal.isOnNext()) {
+							req.mono.onNext(signal.get());
+						} else if (signal.isOnError()) {
+							req.mono.onError(signal.getThrowable());
+						} else if (signal.isOnComplete()) {
+							req.mono.onComplete();
+						}
+
+						Mono.delay(sleepTime).subscribe(l -> {
+							read().subscribe(this);
+							sleepTime = Duration.ZERO;
+						});
+					});
+		}
 	}
 }
--- rest/src/main/java/discord4j/rest/request/Router.java
+++ rest/src/main/java/discord4j/rest/request/Router.java
@@ -1,16 +1,10 @@
 package discord4j.rest.request;
 
-import discord4j.rest.http.client.ExchangeFilter;
 import discord4j.rest.http.client.SimpleHttpClient;
-import io.netty.handler.codec.http.HttpHeaders;
 import reactor.core.publisher.Mono;
 
-import java.time.Duration;
-import java.time.OffsetDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Consumer;
 
 public class Router {
 
@@ -32,55 +26,9 @@ public class Router {
 	@SuppressWarnings("unchecked")
 	private <T> RequestStream<T> getStream(DiscordRequest<T> request) {
 		return (RequestStream<T>) streamMap.computeIfAbsent(request.getBucket(), k -> {
-			RequestStream<T> stream = new RequestStream<>();
-			stream.read().subscribe(new StreamConsumer<>(stream));
+			RequestStream<T> stream = new RequestStream<>(httpClient);
+			stream.start();
 			return stream;
 		});
-	}
-
-	class StreamConsumer<T> implements Consumer<DiscordRequest<T>> {
-
-		private final RequestStream<T> stream;
-		private volatile Duration sleepTime = Duration.ZERO;
-		private final ExchangeFilter exchangeFilter = ExchangeFilter.builder()
-				.responseFilter(response -> {
-					HttpHeaders headers = response.responseHeaders();
-
-					int remaining = headers.getInt("X-RateLimit-Remaining");
-					if (remaining == 0) {
-						long resetAt = Long.parseLong(headers.get("X-RateLimit-Reset"));
-						long discordTime = OffsetDateTime.parse(headers.get("date"), DateTimeFormatter
-								.RFC_1123_DATE_TIME).toEpochSecond();
-						sleepTime = Duration.ofSeconds(resetAt - discordTime);
-					}
-				})
-				.build();
-
-		StreamConsumer(RequestStream<T> stream) {
-			this.stream = stream;
-		}
-
-		@SuppressWarnings("ConstantConditions")
-		@Override
-		public void accept(DiscordRequest<T> req) {
-			httpClient.exchange(req.getMethod(), req.getUri(), req.getBody(), req.getResponseType(), exchangeFilter)
-					.materialize()
-					.subscribe(signal -> {
-						if (signal.isOnSubscribe()) {
-							req.mono.onSubscribe(signal.getSubscription());
-						} else if (signal.isOnNext()) {
-							req.mono.onNext(signal.get());
-						} else if (signal.isOnError()) {
-							req.mono.onError(signal.getThrowable());
-						} else if (signal.isOnComplete()) {
-							req.mono.onComplete();
-						}
-
-						Mono.delay(sleepTime).subscribe(l -> {
-							stream.read().subscribe(this);
-							sleepTime = Duration.ZERO;
-						});
-					});
-		}
 	}
 }
