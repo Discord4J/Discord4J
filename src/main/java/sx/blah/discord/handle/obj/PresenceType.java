1aeba99973d9a903d0cc71e1cb4548466705e0a0
Gerber Lóránt Viktor <glorantq@gmail.com>
2017-11-19 01:48:39 +0100

    Added javadoc comments

--- src/main/java/sx/blah/discord/handle/obj/PresenceType.java
+++ src/main/java/sx/blah/discord/handle/obj/PresenceType.java
@@ -1,5 +1,27 @@
 package sx.blah.discord.handle.obj;
 
+/**
+ * The types a presence can have.
+ */
 public enum PresenceType {
-	PLAYING, STREAMING, WATCHING, LISTENING
+	/**
+	 * The type of playing.
+	 * Displays: Playing xxx
+	 */
+	PLAYING,
+	/**
+	 * The type of streaming.
+	 * Displays: Streaming xxx
+	 */
+	STREAMING,
+	/**
+	 * The type of watching.
+	 * Displays: Watching xxx
+	 */
+	WATCHING,
+	/**
+	 * The type of listening.
+	 * Displays: Listening to xxx
+	 */
+	LISTENING
 }
