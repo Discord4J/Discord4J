7fad6dac5fa5225115c01149e0103d53e6049977
danthonywalker <thedanthonywalker@gmail.com>
2018-04-03 15:42:09 -0600

    Cleaned up DiscordClient and added Javadocs

--- core/src/main/java/discord4j/core/DiscordClient.java
+++ core/src/main/java/discord4j/core/DiscordClient.java
@@ -16,28 +16,147 @@
  */
 package discord4j.core;
 
+import discord4j.common.json.response.ChannelResponse;
 import discord4j.core.object.Snowflake;
 import discord4j.core.object.entity.*;
 import discord4j.core.object.entity.bean.*;
 import discord4j.store.util.LongLongTuple2;
 import reactor.core.publisher.Mono;
 
+/** A high-level abstraction of common Discord operations such as entity retrieval and Discord shard manipulation. */
 public final class DiscordClient {
 
+    /** The ServiceMediator associated to this object. */
     private final ServiceMediator serviceMediator;
-    private final StoreHolder storeHolder;
 
+    /**
+     * Constructs a {@code DiscordClient} with an associated ServiceMediator.
+     *
+     * @param serviceMediator The ServiceMediator associated to this object.
+     */
     DiscordClient(final ServiceMediator serviceMediator) {
         this.serviceMediator = serviceMediator;
-        storeHolder = serviceMediator.getStoreHolder();
     }
 
+    /**
+     * Requests to retrieve the category represented by the supplied ID.
+     *
+     * @param categoryId The ID of the category.
+     * @return A {@link Mono} where, upon successful completion, emits the {@link Category} as represented by the
+     * supplied ID. If an error is received, it is emitted through the {@code Mono}.
+     */
     public Mono<Category> getCategoryById(final Snowflake categoryId) {
         return getChannelById(categoryId).ofType(Category.class);
     }
 
+    /**
+     * Requests to retrieve the guild channel represented by the supplied ID.
+     *
+     * @param guildChannelId The ID of the guild channel.
+     * @return A {@link Mono} where, upon successful completion, emits the {@link GuildChannel} as represented by the
+     * supplied ID. If an error is received, it is emitted through the {@code Mono}.
+     */
+    public Mono<GuildChannel> getGuildChannelById(final Snowflake guildChannelId) {
+        return getChannelById(guildChannelId).ofType(GuildChannel.class);
+    }
+
+    /**
+     * Requests to retrieve the message channel represented by the supplied ID.
+     *
+     * @param messageChannelId The ID of the message channel.
+     * @return A {@link Mono} where, upon successful completion, emits the {@link MessageChannel} as represented by the
+     * supplied ID. If an error is received, it is emitted through the {@code Mono}.
+     */
+    public Mono<MessageChannel> getMessageChannelById(final Snowflake messageChannelId) {
+        return getChannelById(messageChannelId).ofType(MessageChannel.class);
+    }
+
+    /**
+     * Requests to retrieve the text channel represented by the supplied ID.
+     *
+     * @param textChannelId The ID of the text channel.
+     * @return A {@link Mono} where, upon successful completion, emits the {@link TextChannel} as represented by the
+     * supplied ID. If an error is received, it is emitted through the {@code Mono}.
+     */
+    public Mono<TextChannel> getTextChannelById(final Snowflake textChannelId) {
+        return getChannelById(textChannelId).ofType(TextChannel.class);
+    }
+
+    /**
+     * Requests to retrieve the voice channel represented by the supplied ID.
+     *
+     * @param voiceChannelId The ID of the voice channel.
+     * @return A {@link Mono} where, upon successful completion, emits the {@link VoiceChannel} as represented by the
+     * supplied ID. If an error is received, it is emitted through the {@code Mono}.
+     */
+    public Mono<VoiceChannel> getVoiceChannelById(final Snowflake voiceChannelId) {
+        return getChannelById(voiceChannelId).ofType(VoiceChannel.class);
+    }
+
+    /**
+     * Requests to retrieve the channel represented by the supplied ID.
+     *
+     * @param channelId The ID of the channel.
+     * @return A {@link Mono} where, upon successful completion, emits the {@link Channel} as represented by the
+     * supplied ID. If an error is received, it is emitted through the {@code Mono}.
+     */
+    public Mono<Channel> getChannelById(final Snowflake channelId) {
+        final Mono<CategoryBean> category = serviceMediator.getStoreHolder().getCategoryStore().find(channelId.asLong());
+        final Mono<TextChannelBean> textChannel = serviceMediator.getStoreHolder().getTextChannelStore().find(channelId.asLong());
+        final Mono<VoiceChannelBean> voiceChannel = serviceMediator.getStoreHolder().getVoiceChannelStore().find(channelId.asLong());
+
+        final Mono<ChannelBean> rest = serviceMediator.getRestClient().getChannelService()
+                .getChannel(channelId.asLong())
+                .map(this::getChannelBean);
+
+        return category.cast(ChannelBean.class)
+                .switchIfEmpty(textChannel)
+                .switchIfEmpty(voiceChannel)
+                .switchIfEmpty(rest)
+                .map(this::getChannel);
+    }
+
+    /**
+     * An utility that converts a {@code ChannelResponse} to some instance of {@code ChannelBean}.
+     *
+     * @param response The {@code ChannelResponse} to convert.
+     * @return The converted {@code ChannelBean}.
+     */
+    private ChannelBean getChannelBean(final ChannelResponse response) {
+        switch (Channel.Type.of(response.getType())) {
+            case GUILD_TEXT: return new TextChannelBean(response);
+            case DM: return new PrivateChannelBean(response);
+            case GUILD_VOICE: return new VoiceChannelBean(response);
+            case GUILD_CATEGORY: return new CategoryBean(response);
+            default: throw new UnsupportedOperationException("Unknown Response: " + response);
+        }
+    }
+
+    /**
+     * An utility that converts a {@code ChannelBean} to some instance of {@code Channel}.
+     *
+     * @param bean The {@code ChannelBean} to convert.
+     * @return The converted {@code Channel}.
+     */
+    private Channel getChannel(final ChannelBean bean) {
+        switch (Channel.Type.of(bean.getType())) {
+            case GUILD_TEXT: return new TextChannel(serviceMediator, (TextChannelBean) bean);
+            case DM: return new PrivateChannel(serviceMediator, (PrivateChannelBean) bean);
+            case GUILD_VOICE: return new VoiceChannel(serviceMediator, (VoiceChannelBean) bean);
+            case GUILD_CATEGORY: return new Category(serviceMediator, (CategoryBean) bean);
+            default: throw new UnsupportedOperationException("Unknown Bean: " + bean);
+        }
+    }
+
+    /**
+     * Requests to retrieve the guild represented by the supplied ID.
+     *
+     * @param guildId The ID of the guild.
+     * @return A {@link Mono} where, upon successful completion, emits the {@link Guild} as represented by the supplied
+     * ID. If an error is received, it is emitted through the {@code Mono}.
+     */
     public Mono<Guild> getGuildById(final Snowflake guildId) {
-        return storeHolder.getGuildStore()
+        return serviceMediator.getStoreHolder().getGuildStore()
                 .find(guildId.asLong())
                 .cast(BaseGuildBean.class)
                 .switchIfEmpty(serviceMediator.getRestClient().getGuildService()
@@ -46,12 +165,16 @@ public final class DiscordClient {
                 .map(baseGuildBean -> new Guild(serviceMediator, baseGuildBean));
     }
 
-    public Mono<GuildChannel> getGuildChannelById(final Snowflake guildChannelId) {
-        return getChannelById(guildChannelId).ofType(GuildChannel.class);
-    }
-
+    /**
+     * Requests to retrieve the guild emoji represented by the supplied IDs.
+     *
+     * @param guildId The ID of the guild.
+     * @param emojiId The ID of the emoji.
+     * @return A {@link Mono} where, upon successful completion, emits the {@link GuildEmoji} as represented by the
+     * supplied IDs. If an error is received, it is emitted through the {@code Mono}.
+     */
     public Mono<GuildEmoji> getGuildEmojiById(final Snowflake guildId, final Snowflake emojiId) {
-        return storeHolder.getGuildEmojiStore()
+        return serviceMediator.getStoreHolder().getGuildEmojiStore()
                 .find(emojiId.asLong())
                 .switchIfEmpty(serviceMediator.getRestClient().getEmojiService()
                         .getGuildEmoji(guildId.asLong(), emojiId.asLong())
@@ -59,25 +182,35 @@ public final class DiscordClient {
                 .map(guildEmojiBean -> new GuildEmoji(serviceMediator, guildEmojiBean, guildId.asLong()));
     }
 
+    /**
+     * Requests to retrieve the member represented by the supplied IDs.
+     *
+     * @param guildId The ID of the guild.
+     * @param userId The ID of the user.
+     * @return A {@link Mono} where, upon successful completion, emits the {@link Member} as represented by the supplied
+     * IDs. If an error is received, it is emitted through the {@code Mono}.
+     */
     public Mono<Member> getMemberById(final Snowflake guildId, final Snowflake userId) {
-        final Mono<MemberBean> member = storeHolder.getMemberStore()
+        final Mono<MemberBean> member = serviceMediator.getStoreHolder().getMemberStore()
                 .find(LongLongTuple2.of(guildId.asLong(), userId.asLong()))
                 .switchIfEmpty(serviceMediator.getRestClient().getGuildService()
                         .getGuildMember(guildId.asLong(), userId.asLong())
                         .map(MemberBean::new));
 
-        final Mono<UserBean> user = storeHolder.getUserStore()
-                .find(userId.asLong())
-                .switchIfEmpty(serviceMediator.getRestClient().getUserService()
-                        .getUser(userId.asLong())
-                        .map(UserBean::new));
-
-        return member.flatMap(memberBean -> user.map(userBean ->
+        return member.flatMap(memberBean -> getUserBean(userId).map(userBean ->
                 new Member(serviceMediator, memberBean, userBean, guildId.asLong())));
     }
 
+    /**
+     * Requests to retrieve the message represented by the supplied IDs.
+     *
+     * @param channelId The ID of the channel.
+     * @param messageId The ID of the message.
+     * @return A {@link Mono} where, upon successful completion, emits the {@link Message} as represented by the
+     * supplied IDs. If an error is received, it is emitted through the {@code Mono}.
+     */
     public Mono<Message> getMessageById(final Snowflake channelId, final Snowflake messageId) {
-        return storeHolder.getMessageStore()
+        return serviceMediator.getStoreHolder().getMessageStore()
                 .find(messageId.asLong())
                 .switchIfEmpty(serviceMediator.getRestClient().getChannelService()
                         .getMessage(channelId.asLong(), messageId.asLong())
@@ -85,12 +218,16 @@ public final class DiscordClient {
                 .map(messageBean -> new Message(serviceMediator, messageBean));
     }
 
-    public Mono<MessageChannel> getMessageChannelById(final Snowflake messageChannelId) {
-        return getChannelById(messageChannelId).ofType(MessageChannel.class);
-    }
-
+    /**
+     * Requests to retrieve the role represented by the supplied IDs.
+     *
+     * @param guildId The ID of the guild.
+     * @param roleId The ID of the role.
+     * @return A {@link Mono} where, upon successful completion, emits the {@link Role} as represented by the supplied
+     * IDs. If an error is received, it is emitted through the {@code Mono}.
+     */
     public Mono<Role> getRoleById(final Snowflake guildId, final Snowflake roleId) {
-        return storeHolder.getRoleStore()
+        return serviceMediator.getStoreHolder().getRoleStore()
                 .find(roleId.asLong())
                 .switchIfEmpty(serviceMediator.getRestClient().getGuildService()
                         .getGuildRoles(guildId.asLong())
@@ -100,58 +237,43 @@ public final class DiscordClient {
                 .map(roleBean -> new Role(serviceMediator, roleBean, guildId.asLong()));
     }
 
-    public Mono<TextChannel> getTextChannelById(final Snowflake textChannelId) {
-        return getChannelById(textChannelId).ofType(TextChannel.class);
-    }
-
+    /**
+     * Requests to retrieve the user represented by the supplied ID.
+     *
+     * @param userId The ID of the user.
+     * @return A {@link Mono} where, upon successful completion, emits the {@link User} as represented by the supplied
+     * ID. If an error is received, it is emitted through the {@code Mono}.
+     */
     public Mono<User> getUserById(final Snowflake userId) {
-        return storeHolder.getUserStore()
-                .find(userId.asLong())
-                .switchIfEmpty(serviceMediator.getRestClient().getUserService()
-                        .getUser(userId.asLong())
-                        .map(UserBean::new))
-                .map(userBean -> new User(serviceMediator, userBean));
+        return getUserBean(userId).map(userBean -> new User(serviceMediator, userBean));
     }
 
-    public Mono<VoiceChannel> getVoiceChannelById(final Snowflake voiceChannelId) {
-        return getChannelById(voiceChannelId).ofType(VoiceChannel.class);
-    }
-
+    /**
+     * Requests to retrieve the webhook represented by the supplied ID.
+     *
+     * @param webhookId The ID of the webhook.
+     * @return A {@link Mono} where, upon successful completion, emits the {@link Webhook} as represented by the
+     * supplied ID. If an error is received, it is emitted through the {@code Mono}.
+     */
     public Mono<Webhook> getWebhookById(final Snowflake webhookId) {
         return serviceMediator.getRestClient().getWebhookService()
                 .getWebhook(webhookId.asLong())
                 .map(WebhookBean::new)
-                .map(bean -> new Webhook(serviceMediator, bean));
+                .map(webhookBean -> new Webhook(serviceMediator, webhookBean));
     }
 
-    private Mono<Channel> getChannelById(final Snowflake channelId) {
-        final Mono<Category> category = storeHolder.getCategoryStore()
-                .find(channelId.asLong())
-                .map(categoryBean -> new Category(serviceMediator, categoryBean));
-
-        final Mono<TextChannel> textChannel = storeHolder.getTextChannelStore()
-                .find(channelId.asLong())
-                .map(textChannelBean -> new TextChannel(serviceMediator, textChannelBean));
-
-        final Mono<VoiceChannel> voiceChannel = storeHolder.getVoiceChannelStore()
-                .find(channelId.asLong())
-                .map(voiceChannelBean -> new VoiceChannel(serviceMediator, voiceChannelBean));
-
-        final Mono<Channel> rest = serviceMediator.getRestClient().getChannelService()
-                .getChannel(channelId.asLong())
-                .map(channelResponse -> {
-                    switch (Channel.Type.of(channelResponse.getType())) {
-                        case GUILD_TEXT: return new TextChannel(serviceMediator, new TextChannelBean(channelResponse));
-                        case DM: return new PrivateChannel(serviceMediator, new PrivateChannelBean(channelResponse));
-                        case GUILD_VOICE: return new VoiceChannel(serviceMediator, new VoiceChannelBean(channelResponse));
-                        case GUILD_CATEGORY: return new Category(serviceMediator, new CategoryBean(channelResponse));
-                        default: throw new UnsupportedOperationException("Cannot initialize channel for " + channelId);
-                    }
-                });
-
-        return category.cast(Channel.class)
-                .switchIfEmpty(textChannel)
-                .switchIfEmpty(voiceChannel)
-                .switchIfEmpty(rest);
+    /**
+     * Requests to retrieve the user bean represented by the supplied ID.
+     *
+     * @param userId The ID of the user.
+     * @return A {@link Mono} where, upon successful completion, emits the {@link UserBean} as represented by the
+     * supplied ID. If an error is received, it is emitted through the {@code Mono}.
+     */
+    private Mono<UserBean> getUserBean(final Snowflake userId) {
+        return serviceMediator.getStoreHolder().getUserStore()
+                .find(userId.asLong())
+                .switchIfEmpty(serviceMediator.getRestClient().getUserService()
+                        .getUser(userId.asLong())
+                        .map(UserBean::new));
     }
 }
