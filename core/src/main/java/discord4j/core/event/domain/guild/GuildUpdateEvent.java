094988790faa7f45e5dc77560ae4154864222a16
GrandPanda <darichey1@gmail.com>
2018-04-16 21:30:58 -0500

    Remove Update<T>, add nullability annotations

--- core/src/main/java/discord4j/core/event/domain/guild/GuildUpdateEvent.java
+++ core/src/main/java/discord4j/core/event/domain/guild/GuildUpdateEvent.java
@@ -17,104 +17,27 @@
 package discord4j.core.event.domain.guild;
 
 import discord4j.core.DiscordClient;
-import discord4j.core.event.Update;
 import discord4j.core.object.entity.Guild;
-import discord4j.core.object.util.Snowflake;
 
 import javax.annotation.Nullable;
 import java.util.Optional;
-import java.util.Set;
 
 public class GuildUpdateEvent extends GuildEvent {
 
-    private final Update<Snowflake> afkChannel;
-    private final Update<Integer> afkTimeout;
-    private final Update<Guild.ContentFilterLevel> contentFilterLevel;
-    private final Update<Set<String>> features;
-    private final Update<String> iconHash;
-    private final Update<Guild.MfaLevel> mfaLevel;
-    private final Update<String> name;
-    private final Update<Guild.NotificationLevel> notificationLevel;
-    private final Update<Snowflake> owner;
-    private final Update<String> region;
-    private final Update<String> splash;
-    private final Update<Snowflake> systemChannel;
-    private final Update<Guild.VerificationLevel> verificationLevel;
+    private final Guild current;
+    private final Guild old;
 
-    public GuildUpdateEvent(DiscordClient client, @Nullable Update<Snowflake> afkChannel,
-                            @Nullable Update<Integer> afkTimeout,
-                            @Nullable Update<Guild.ContentFilterLevel> contentFilterLevel,
-                            @Nullable Update<Set<String>> features, @Nullable Update<String> iconHash,
-                            @Nullable Update<Guild.MfaLevel> mfaLevel, Update<String> name,
-                            @Nullable Update<Guild.NotificationLevel> notificationLevel, Update<Snowflake> owner,
-                            @Nullable Update<String> region, @Nullable Update<String> splash,
-                            @Nullable Update<Snowflake> systemChannel,
-                            @Nullable Update<Guild.VerificationLevel> verificationLevel) {
+    public GuildUpdateEvent(DiscordClient client, Guild current, @Nullable Guild old) {
         super(client);
-        this.afkChannel = afkChannel;
-        this.afkTimeout = afkTimeout;
-        this.contentFilterLevel = contentFilterLevel;
-        this.features = features;
-        this.iconHash = iconHash;
-        this.mfaLevel = mfaLevel;
-        this.name = name;
-        this.notificationLevel = notificationLevel;
-        this.owner = owner;
-        this.region = region;
-        this.splash = splash;
-        this.systemChannel = systemChannel;
-        this.verificationLevel = verificationLevel;
+        this.current = current;
+        this.old = old;
     }
 
-    public Optional<Update<Snowflake>> getAfkChannel() {
-        return Optional.ofNullable(afkChannel);
+    public Guild getCurrent() {
+        return current;
     }
 
-    public Optional<Update<Integer>> getAfkTimeout() {
-        return Optional.ofNullable(afkTimeout);
-    }
-
-    public Optional<Update<Guild.ContentFilterLevel>> getContentFilterLevel() {
-        return Optional.ofNullable(contentFilterLevel);
-    }
-
-    public Optional<Update<Set<String>>> getFeatures() {
-        return Optional.ofNullable(features);
-    }
-
-    public Optional<Update<String>> getIconHash() {
-        return Optional.ofNullable(iconHash);
-    }
-
-    public Optional<Update<Guild.MfaLevel>> getMfaLevel() {
-        return Optional.ofNullable(mfaLevel);
-    }
-
-    public Optional<Update<String>> getName() {
-        return Optional.ofNullable(name);
-    }
-
-    public Optional<Update<Guild.NotificationLevel>> getNotificationLevel() {
-        return Optional.ofNullable(notificationLevel);
-    }
-
-    public Optional<Update<Snowflake>> getOwner() {
-        return Optional.ofNullable(owner);
-    }
-
-    public Optional<Update<String>> getRegion() {
-        return Optional.ofNullable(region);
-    }
-
-    public Optional<Update<String>> getSplash() {
-        return Optional.ofNullable(splash);
-    }
-
-    public Optional<Update<Snowflake>> getSystemChannel() {
-        return Optional.ofNullable(systemChannel);
-    }
-
-    public Optional<Update<Guild.VerificationLevel>> getVerificationLevel() {
-        return Optional.ofNullable(verificationLevel);
+    public Optional<Guild> getOld() {
+        return Optional.ofNullable(old);
     }
 }
