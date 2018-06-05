7650ad528a407c84b55093b0703626af3a7c5125
GrandPanda <darichey1@gmail.com>
2018-06-05 17:55:54 -0500

    Fix discord4j.core.presence package nullability

--- core/src/main/java/discord4j/core/object/presence/package-info.java
+++ core/src/main/java/discord4j/core/object/presence/package-info.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Discord4J.  If not, see <http://www.gnu.org/licenses/>.
  */
-@Nullable
+@NonNullApi
 package discord4j.core.object.presence;
 
-import javax.annotation.Nullable;
+import reactor.util.annotation.NonNullApi;
