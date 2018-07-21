5e6a1b42939003bac2c94f05fd31bd0bf10ccb52
austinv11 <austinv11@gmail.com>
2018-07-21 10:47:33 -0400

    Update the examples

--- commands/src/test/java/discord4j/commands/example/LogoutCommand.java
+++ commands/src/test/java/discord4j/commands/example/LogoutCommand.java
@@ -3,21 +3,32 @@ package discord4j.commands.example;
 import discord4j.commands.BaseCommand;
 import discord4j.commands.CommandException;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Member;
+import discord4j.core.object.util.Snowflake;
 import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 
 public class LogoutCommand implements BaseCommand {
 
+    private final Snowflake ownerId;
+
+    public LogoutCommand(Snowflake ownerId) {
+        this.ownerId = ownerId;
+    }
+
     @Override
     public Mono<Void> execute(MessageCreateEvent event) {
-        return event.getClient()
-                    .getApplicationInfo()
-                    .filter(info -> info.getOwnerId().equals(event.getMessage().getAuthorId().get()))
-                    .switchIfEmpty(Mono.fromRunnable(() -> {
-                        throw new CommandException("You're not my owner!");
-                    }))
-                    .doOnSuccess(i -> event.getClient().logout())
-                    .then();
+        return Mono.just(event)
+                .map(MessageCreateEvent::getMember)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .map(Member::getId)
+                .filter(memberId -> memberId.equals(ownerId))
+                .switchIfEmpty(Mono.fromRunnable(() -> {
+                    throw new CommandException("You're not my owner!");
+                }))
+                .doOnSuccess(i -> event.getClient().logout())
+                .then();
     }
 }
--- commands/src/test/java/discord4j/commands/example/SimpleCommandProvider.java
+++ commands/src/test/java/discord4j/commands/example/SimpleCommandProvider.java
@@ -2,11 +2,22 @@ package discord4j.commands.example;
 
 import discord4j.commands.BaseCommand;
 import discord4j.commands.CommandProvider;
+import discord4j.core.DiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.ApplicationInfo;
+import discord4j.core.object.util.Snowflake;
 
 import java.util.Optional;
 
 public class SimpleCommandProvider implements CommandProvider {
+
+    private final DiscordClient client;
+    private final Snowflake ownerId;
+
+    public SimpleCommandProvider(DiscordClient client) {
+        this.client = client;
+        this.ownerId = client.getApplicationInfo().map(ApplicationInfo::getOwnerId).block();
+    }
 
     @Override
     public Optional<? extends BaseCommand> provide(MessageCreateEvent context) {
@@ -18,7 +29,7 @@ public class SimpleCommandProvider implements CommandProvider {
                           if (cmd.equalsIgnoreCase("echo"))
                               return new EchoCommand();
                           else if (cmd.equalsIgnoreCase("logout"))
-                              return new LogoutCommand();
+                              return new LogoutCommand(ownerId);
                           else
                               return null;
                       });
