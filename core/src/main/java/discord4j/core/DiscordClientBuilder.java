c111207144f63c7dec16da750e3b0a6b44f78604
quanticc <quanticc@users.noreply.github.com>
2018-11-01 22:11:44 -0300

    Fix DiscordClientBuilder reuse issues
    Basically do not perform any mutation inside build method,
    allowing reuse without side-effects under same-process sharding.

--- core/src/main/java/discord4j/core/DiscordClientBuilder.java
+++ core/src/main/java/discord4j/core/DiscordClientBuilder.java
@@ -21,12 +21,12 @@ import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import discord4j.common.jackson.PossibleModule;
+import discord4j.common.jackson.UnknownPropertyHandler;
 import discord4j.core.event.EventDispatcher;
 import discord4j.core.event.dispatch.DispatchContext;
 import discord4j.core.event.dispatch.DispatchHandlers;
 import discord4j.core.event.domain.Event;
 import discord4j.core.object.presence.Presence;
-import discord4j.common.jackson.UnknownPropertyHandler;
 import discord4j.core.util.VersionUtil;
 import discord4j.gateway.GatewayClient;
 import discord4j.gateway.IdentifyOptions;
@@ -65,30 +65,29 @@ public final class DiscordClientBuilder {
     private static final Logger log = Loggers.getLogger(DiscordClientBuilder.class);
 
     private String token;
+
     private int shardIndex = 0;
+
     private int shardCount = 1;
-    private StoreServiceLoader storeServiceLoader;
+
     @Nullable
     private StoreService storeService = null;
+
     private FluxProcessor<Event, Event> eventProcessor;
+
     private Scheduler eventScheduler;
+
     private Presence initialPresence;
+
     @Nullable
     private IdentifyOptions identifyOptions;
+
     private RetryOptions retryOptions;
-    private boolean ignoreUnknownJsonKeys;
+
+    private boolean ignoreUnknownJsonKeys = true;
 
     public DiscordClientBuilder(final String token) {
         this.token = Objects.requireNonNull(token);
-        //Increase JDK store priority by default
-        Map<Class<? extends StoreService>, Integer> priority = new HashMap<>();
-        // We want almost minimum priority, so that jdk can beat no-op but most implementations will beat jdk
-        priority.put(JdkStoreService.class, (int) (Integer.MAX_VALUE - 1));
-        storeServiceLoader = new StoreServiceLoader(priority);
-        eventProcessor = EmitterProcessor.create(false);
-        eventScheduler = Schedulers.elastic();
-        retryOptions = new RetryOptions(Duration.ofSeconds(2), Duration.ofSeconds(120), Integer.MAX_VALUE);
-        ignoreUnknownJsonKeys = true;
     }
 
     public String getToken() {
@@ -182,63 +181,100 @@ public final class DiscordClientBuilder {
         return this;
     }
 
+    private IdentifyOptions initIdentifyOptions() {
+        if (identifyOptions != null) {
+            return identifyOptions;
+        }
+        return new IdentifyOptions(shardIndex, shardCount,
+                initialPresence != null ? initialPresence.asStatusUpdate() : null);
+    }
+
+    private RetryOptions initRetryOptions() {
+        if (retryOptions != null) {
+            return retryOptions;
+        }
+        return new RetryOptions(Duration.ofSeconds(2), Duration.ofSeconds(120), Integer.MAX_VALUE);
+    }
+
+    private StoreService initStoreService() {
+        if (storeService != null) {
+            return storeService;
+        }
+        Map<Class<? extends StoreService>, Integer> priority = new HashMap<>();
+        // We want almost minimum priority, so that jdk can beat no-op but most implementations will beat jdk
+        priority.put(JdkStoreService.class, Integer.MAX_VALUE - 1);
+        StoreServiceLoader storeServiceLoader = new StoreServiceLoader(priority);
+        return storeServiceLoader.getStoreService();
+    }
+
+    private FluxProcessor<Event, Event> initEventProcessor() {
+        if (eventProcessor != null) {
+            return eventProcessor;
+        }
+        return EmitterProcessor.create(false);
+    }
+
+    private Scheduler initEventScheduler() {
+        if (eventScheduler != null) {
+            return eventScheduler;
+        }
+        return Schedulers.elastic();
+    }
+
     public DiscordClient build() {
         final ObjectMapper mapper = new ObjectMapper()
                 .setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
                 .addHandler(new UnknownPropertyHandler(ignoreUnknownJsonKeys))
                 .registerModules(new PossibleModule(), new Jdk8Module());
 
-        final Properties properties = VersionUtil.getProperties();
-        final String version = properties.getProperty(VersionUtil.APPLICATION_VERSION, "3");
-        final String url = properties.getProperty(VersionUtil.APPLICATION_URL, "https://discord4j.com");
-
-        HttpHeaders defaultHeaders = new DefaultHttpHeaders();
-        defaultHeaders.add(HttpHeaderNames.CONTENT_TYPE, "application/json");
-        defaultHeaders.add(HttpHeaderNames.AUTHORIZATION, "Bot " + token);
-        defaultHeaders.add(HttpHeaderNames.USER_AGENT, "DiscordBot(" + url + ", " + version + ")");
-        HttpClient httpClient = HttpClient.create().baseUrl(Routes.BASE_URL).compress(true);
-        DiscordWebClient webClient = new DiscordWebClient(httpClient, defaultHeaders,
-                ExchangeStrategies.withJacksonDefaults(mapper));
-
-        if (identifyOptions == null) {
-            identifyOptions = new IdentifyOptions(shardIndex, shardCount,
-                    initialPresence != null ? initialPresence.asStatusUpdate() : null);
+        // Prepare GatewayClient
+        final IdentifyOptions identifyOptions = initIdentifyOptions();
+        if (identifyOptions.getShardIndex() < 0 || identifyOptions.getShardIndex() >= identifyOptions.getShardCount()) {
+            throw new IllegalArgumentException("0 <= shardIndex < shardCount");
         }
-
-        if (identifyOptions.getShardIndex() >= identifyOptions.getShardCount()) {
-            throw new IllegalArgumentException("The provided shard index is incompatible with the requested shard " +
-                    "count!");
-        }
-
+        final RetryOptions retryOptions = initRetryOptions();
         final GatewayClient gatewayClient = new GatewayClient(
                 new JacksonPayloadReader(mapper), new JacksonPayloadWriter(mapper),
                 retryOptions, token, identifyOptions);
 
-        if (storeService == null) {
-            storeService = storeServiceLoader.getStoreService();
-        }
+        // Retrieve version properties
+        final Properties properties = VersionUtil.getProperties();
+        final String version = properties.getProperty(VersionUtil.APPLICATION_VERSION, "3");
+        final String url = properties.getProperty(VersionUtil.APPLICATION_URL, "https://discord4j.com");
+        final String name = properties.getProperty(VersionUtil.APPLICATION_NAME, "Discord4J");
+        final String gitDescribe = properties.getProperty(VersionUtil.GIT_COMMIT_ID_DESCRIBE, version);
 
-        final StateHolder stateHolder = new StateHolder(storeService);
+        // Prepare RestClient
+        final HttpHeaders defaultHeaders = new DefaultHttpHeaders();
+        defaultHeaders.add(HttpHeaderNames.CONTENT_TYPE, "application/json");
+        defaultHeaders.add(HttpHeaderNames.AUTHORIZATION, "Bot " + token);
+        defaultHeaders.add(HttpHeaderNames.USER_AGENT, "DiscordBot(" + url + ", " + version + ")");
+        final HttpClient httpClient = HttpClient.create().baseUrl(Routes.BASE_URL).compress(true);
+        final DiscordWebClient webClient = new DiscordWebClient(httpClient, defaultHeaders,
+                ExchangeStrategies.withJacksonDefaults(mapper));
         final RestClient restClient = new RestClient(new Router(webClient, Schedulers.elastic()));
+
+        // Prepare Stores
+        final StoreService storeService = initStoreService();
+        final StateHolder stateHolder = new StateHolder(storeService);
+
+        // Prepare EventDispatcher
+        final FluxProcessor<Event, Event> eventProcessor = initEventProcessor();
+        final EventDispatcher eventDispatcher = new EventDispatcher(eventProcessor, initEventScheduler());
+
+        // Prepare identify parameters
         final ClientConfig config = new ClientConfig(token, identifyOptions.getShardIndex(),
                 identifyOptions.getShardCount());
-        final EventDispatcher eventDispatcher = new EventDispatcher(eventProcessor, eventScheduler);
 
+        // Prepare mediator and wire gateway events to EventDispatcher
         final ServiceMediator serviceMediator = new ServiceMediator(gatewayClient, restClient, storeService,
                 stateHolder, eventDispatcher, config);
-
         serviceMediator.getGatewayClient().dispatch()
                 .map(dispatch -> DispatchContext.of(dispatch, serviceMediator))
                 .flatMap(DispatchHandlers::<Dispatch, Event>handle)
                 .subscribeWith(eventProcessor);
 
-        final String name = properties.getProperty(VersionUtil.APPLICATION_NAME, "Discord4J");
-        final String gitDescribe = properties.getProperty(VersionUtil.GIT_COMMIT_ID_DESCRIBE, version);
-        final String description = properties.getProperty(VersionUtil.APPLICATION_DESCRIPTION);
-        log.info("{} {} ({})", name, gitDescribe, url);
-        if (description != null) {
-            log.info("{}", description);
-        }
+        log.info("Shard {} with {} {} ({})", identifyOptions.getShardIndex(), name, gitDescribe, url);
         return serviceMediator.getClient();
     }
 }
