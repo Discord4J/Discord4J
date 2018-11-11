07273f120025261f6595804239eda8cef1a9c7da
quanticc <quanticc@users.noreply.github.com>
2018-11-11 13:51:42 -0300

    Change Flux.fromStream usage to take Supplier

--- core/src/main/java/discord4j/core/event/dispatch/GuildDispatchHandlers.java
+++ core/src/main/java/discord4j/core/event/dispatch/GuildDispatchHandlers.java
@@ -140,9 +140,9 @@ class GuildDispatchHandlers {
         return stateHolder.getGuildStore()
                 .find(context.getDispatch().getGuild().getId())
                 .flatMap(guild -> {
-                    Flux<Long> channels = Flux.fromStream(LongStream.of(guild.getChannels()).boxed());
-                    Flux<Long> roles = Flux.fromStream(LongStream.of(guild.getRoles()).boxed());
-                    Flux<Long> emojis = Flux.fromStream(LongStream.of(guild.getEmojis()).boxed());
+                    Flux<Long> channels = Flux.fromStream(() -> LongStream.of(guild.getChannels()).boxed());
+                    Flux<Long> roles = Flux.fromStream(() -> LongStream.of(guild.getRoles()).boxed());
+                    Flux<Long> emojis = Flux.fromStream(() -> LongStream.of(guild.getEmojis()).boxed());
 
                     Mono<Void> deleteTextChannels = stateHolder.getTextChannelStore().delete(channels);
                     Mono<Void> deleteVoiceChannels = stateHolder.getVoiceChannelStore().delete(channels);
@@ -267,7 +267,7 @@ class GuildDispatchHandlers {
                 .map(response -> Tuples.of(response, new MemberBean(response)))
                 .map(tuple -> Tuples.of(LongLongTuple2.of(guildId, tuple.getT1().getUser().getId()), tuple.getT2()));
 
-        Flux<Tuple2<Long, UserBean>> userPairs = Flux.fromStream(Arrays.stream(context.getDispatch().getMembers()))
+        Flux<Tuple2<Long, UserBean>> userPairs = Flux.fromStream(() -> Arrays.stream(context.getDispatch().getMembers()))
                 .map(response -> new UserBean(response.getUser()))
                 .map(bean -> Tuples.of(bean.getId(), bean));
 
