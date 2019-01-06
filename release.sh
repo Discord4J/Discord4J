f947316a705d6bd8b4202a3c528361b78a9d799f
quanticc <quanticc@users.noreply.github.com>
2019-01-06 17:44:58 -0300

    Tag repo on successful publish

--- release.sh
+++ release.sh
@@ -5,3 +5,7 @@ if [ -z "$1" ]
     exit 1
 fi
 ./gradlew -Pversion="$1" -PstoresVersion="$1" -PstoresArtifact="com.discord4j:stores-jdk" clean build -x test publishToMavenLocal bintrayUpload
+if [ $? -eq 0 ]
+  then
+    git tag "$1" && git push --tags
+fi
