e14a82362509f7d7ba9b3a41975ee0e6323a2e3a
quanticc <quanticc@users.noreply.github.com>
2019-01-06 17:18:47 -0300

    Update release script

--- release.sh
+++ release.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 if [ -z "$1" ]
   then
     echo "Version argument missing"
     exit 1
 fi
-chmod +x gradlew
 ./gradlew -Pversion="$1" -PstoresVersion="$1" -PstoresArtifact="com.discord4j:stores-jdk" clean build -x test publishToMavenLocal bintrayUpload
