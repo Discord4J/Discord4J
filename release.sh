aff3f0b442a7b1d7380ebd09543ddfaf3f5b3df6
quanticc <quanticc@users.noreply.github.com>
2019-02-04 22:12:34 -0300

    Update release script

--- release.sh
+++ release.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-if [ -z "$1" ]
+if [[ -z "$1" ]]
   then
     echo "Version argument missing"
     exit 1
 fi
-./gradlew -Pversion="$1" -PstoresVersion="$1" -PstoresArtifact="com.discord4j:stores-jdk" clean build -x test publishToMavenLocal bintrayUpload
+VERSION=$1
+if [[ -z "$2" ]]
+  then
+    STORES_VERSION=$1
+else
+  STORES_VERSION=$2
+fi
+echo "Releasing version $VERSION depending on Stores $STORES_VERSION"
+./gradlew build -x test release -Pversion="$VERSION" -PstoresVersion="$STORES_VERSION" -PstoresArtifact="com.discord4j:stores-jdk"
