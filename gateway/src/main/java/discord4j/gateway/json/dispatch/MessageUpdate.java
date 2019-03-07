2ba4410225d59db4f17de371ec85f3fec60b9735
Dylan Curzon <d.46.curzon@hotmail.co.uk>
2019-03-07 16:57:42 +0000

    Add missing attributes to MessageUpdate dispatch

--- gateway/src/main/java/discord4j/gateway/json/dispatch/MessageUpdate.java
+++ gateway/src/main/java/discord4j/gateway/json/dispatch/MessageUpdate.java
@@ -18,43 +18,80 @@ package discord4j.gateway.json.dispatch;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import discord4j.common.jackson.Possible;
-import discord4j.common.jackson.PossibleLong;
 import discord4j.common.jackson.UnsignedJson;
+import discord4j.common.json.AttachmentResponse;
 import discord4j.common.json.EmbedResponse;
+import discord4j.common.json.UserResponse;
 import reactor.util.annotation.Nullable;
+
+import java.util.Arrays;
 
 public class MessageUpdate implements Dispatch {
 
+    private int type;
+    private boolean tts;
+    private Possible<String> timestamp = Possible.absent();
+    private Possible<Boolean> pinned = Possible.absent();
+    @Nullable
+    private String nonce;
+    private Possible<MessageCreate.Mention[]> mentions = Possible.absent();
+    @JsonProperty("mention_roles")
+    private Possible<long[]> mentionRoles = Possible.absent();
+    @JsonProperty("mention_everyone")
+    private Possible<Boolean> mentionEveryone = Possible.absent();
     @UnsignedJson
     private long id;
+    @Nullable
+    private Possible<EmbedResponse[]> embeds = Possible.absent();
+    @JsonProperty("edited_timestamp")
+    private Possible<String> editedTimestamp = Possible.absent();
+    @Nullable
+    private Possible<String> content = Possible.absent();
     @JsonProperty("channel_id")
     @UnsignedJson
     private long channelId;
+    private Possible<UserResponse> author = Possible.absent();
+    private Possible<AttachmentResponse[]> attachments = Possible.absent();
     @JsonProperty("guild_id")
     @UnsignedJson
     @Nullable
     private Long guildId;
+
+    public int getType() {
+        return type;
+    }
+
+    public boolean isTts() {
+        return tts;
+    }
+
+    public Possible<String> getTimestamp() {
+        return timestamp;
+    }
+
+    public Possible<Boolean> isPinned() {
+        return pinned;
+    }
+
     @Nullable
-    private Possible<String> content = Possible.absent();
-    @Nullable
-    private Possible<EmbedResponse[]> embeds = Possible.absent();
+    public String getNonce() {
+        return nonce;
+    }
+
+    public Possible<MessageCreate.Mention[]> getMentions() {
+        return mentions;
+    }
+
+    public Possible<long[]> getMentionRoles() {
+        return mentionRoles;
+    }
+
+    public Possible<Boolean> getMentionEveryone() {
+        return mentionEveryone;
+    }
 
     public long getId() {
         return id;
-    }
-
-    public long getChannelId() {
-        return channelId;
-    }
-
-    @Nullable
-    public Long getGuildId() {
-        return guildId;
-    }
-
-    @Nullable
-    public Possible<String> getContent() {
-        return content;
     }
 
     @Nullable
@@ -62,14 +99,52 @@ public class MessageUpdate implements Dispatch {
         return embeds;
     }
 
+    public Possible<String> getEditedTimestamp() {
+        return editedTimestamp;
+    }
+
+    @Nullable
+    public Possible<String> getContent() {
+        return content;
+    }
+
+    public long getChannelId() {
+        return channelId;
+    }
+
+    public Possible<UserResponse> getAuthor() {
+        return author;
+    }
+
+    public Possible<AttachmentResponse[]> getAttachments() {
+        return attachments;
+    }
+
+    @Nullable
+    public Long getGuildId() {
+        return guildId;
+    }
+
     @Override
     public String toString() {
         return "MessageUpdate{" +
-                "id=" + id +
-                ", channelId=" + channelId +
-                ", guildId=" + guildId +
-                ", content=" + content +
+                "type=" + type +
+                ", tts=" + tts +
+                ", timestamp='" + timestamp + "\'" +
+                ", pinned=" + pinned +
+                ", nonce='" + nonce + "\'" +
+                ", mentions=" + (mentions.isAbsent() ? mentions : Arrays.toString(mentions.get())) +
+                ", mentionRoles=" + (mentionRoles.isAbsent() ? mentionRoles : Arrays.toString(mentionRoles.get())) +
+                ", mentionEveryone=" + mentionEveryone +
+                ", id=" + id +
                 ", embeds=" + embeds +
+                ", editedTimestamp=" + editedTimestamp +
+                ", content=" + content +
+                ", channelId=" + channelId +
+                ", author=" + author +
+                ", attachments=" + (attachments.isAbsent() ? attachments : Arrays.toString(attachments.get())) +
+                ", guildId=" + guildId +
                 '}';
     }
+
 }
