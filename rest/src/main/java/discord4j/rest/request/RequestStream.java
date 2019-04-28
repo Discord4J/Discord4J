Discord4J/Discord4J

commit 635af28eafa7cd5fc0b1f1af44a35a4786a99b72

Allow custom API error handling (#492)
Enable registering client-wide behavior to API responses, allowing users to suppress certain errors or apply custom retry logic across a subset of all requests made by a client.

Can be setup while building a client, through RouterOptions configuration object.

rest/src/main/java/discord4j/rest/request/RequestStream.java
@@ -19,9 +19,6 @@ package discord4j.rest.request;
 import discord4j.rest.http.client.ClientException;
 import discord4j.rest.http.client.ClientRequest;
 import discord4j.rest.http.client.DiscordWebClient;
-import discord4j.rest.util.RouteUtils;
-import io.netty.handler.codec.http.DefaultHttpHeaders;
-import io.netty.handler.codec.http.HttpHeaders;
 import reactor.core.publisher.*;
 import reactor.core.scheduler.Scheduler;
 import reactor.netty.http.client.HttpClientResponse;
@@ -34,7 +31,6 @@ import reactor.util.Loggers;
 import reactor.util.annotation.Nullable;
 
 import java.time.Duration;
-import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.logging.Level;
@@ -59,15 +55,17 @@ class RequestStream<T> {
     private final GlobalRateLimiter globalRateLimiter;
     private final RateLimitStrategy rateLimitStrategy;
     private final Scheduler rateLimitScheduler;
+    private final RouterOptions routerOptions;
 
     RequestStream(BucketKey id, DiscordWebClient httpClient, GlobalRateLimiter globalRateLimiter,
-                  RateLimitStrategy rateLimitStrategy, Scheduler rateLimitScheduler) {
+                  RateLimitStrategy rateLimitStrategy, Scheduler rateLimitScheduler, RouterOptions routerOptions) {
         this.id = id;
         this.log = Loggers.getLogger("discord4j.rest.traces." + id);
         this.httpClient = httpClient;
         this.globalRateLimiter = globalRateLimiter;
         this.rateLimitStrategy = rateLimitStrategy;
         this.rateLimitScheduler = rateLimitScheduler;
+        this.routerOptions = routerOptions;
     }
 
     /**
@@ -112,7 +110,7 @@ class RequestStream<T> {
      * 502, 503 and 504). The delay is calculated using exponential backoff with jitter.
      */
     private Retry<?> serverErrorRetryFactory() {
-        return Retry.onlyIf(this::isServerError)
+        return Retry.onlyIf(ClientException.isRetryContextStatusCode(502, 503, 504))
                 .exponentialBackoffWithJitter(Duration.ofSeconds(2), Duration.ofSeconds(30))
                 .doOnRetry(ctx -> {
                     if (log.isTraceEnabled()) {
@@ -120,17 +118,6 @@ class RequestStream<T> {
                                 ctx.backoff());
                     }
                 });
-    }
-
-    private boolean isServerError(IterationContext<?> context) {
-        RetryContext<?> ctx = (RetryContext) context;
-        Throwable exception = ctx.exception();
-        if (exception instanceof ClientException) {
-            ClientException clientException = (ClientException) exception;
-            int code = clientException.getStatus().code();
-            return code == 502 || code == 503 || code == 504;
-        }
-        return false;
     }
 
     void push(RequestCorrelation<T> request) {
@@ -182,10 +169,11 @@ class RequestStream<T> {
             Logger responseLog = getLogger("response", shard);
             Class<T> responseType = request.getRoute().getResponseType();
 
-            globalRateLimiter.withLimiter(() -> adaptRequest(request)
+            globalRateLimiter.withLimiter(() -> Mono.fromCallable(() -> new ClientRequest(request))
                     .log(requestLog, Level.FINEST, false)
                     .flatMap(r -> httpClient.exchange(r, request.getBody(), responseType, rateLimitHandler))
                     .retryWhen(rateLimitRetryFactory())
+                    .transform(getResponseTransformers(request))
                     .retryWhen(serverErrorRetryFactory())
                     .log(responseLog, Level.FINEST, false)
                     .doFinally(signal -> next(signal, traceLog)))
@@ -203,17 +191,12 @@ class RequestStream<T> {
                     });
         }
 
-        private Mono<ClientRequest> adaptRequest(DiscordRequest<?> req) {
-            return Mono.fromCallable(() -> new ClientRequest(req.getRoute().getMethod(),
-                    RouteUtils.expandQuery(req.getCompleteUri(), req.getQueryParams()),
-                    Optional.ofNullable(req.getHeaders())
-                            .map(map -> map.entrySet().stream()
-                                    .reduce((HttpHeaders) new DefaultHttpHeaders(), (headers, entry) -> {
-                                        String key = entry.getKey();
-                                        entry.getValue().forEach(value -> headers.add(key, value));
-                                        return headers;
-                                    }, HttpHeaders::add))
-                            .orElse(new DefaultHttpHeaders())));
+        private Function<Mono<T>, Mono<T>> getResponseTransformers(DiscordRequest<T> discordRequest) {
+            return routerOptions.getResponseTransformers()
+                    .stream()
+                    .map(rt -> rt.transform(discordRequest))
+                    .reduce(Function::andThen)
+                    .orElse(mono -> mono);
         }
 
         private void next(SignalType signal, Logger logger) {

core/src/main/java/discord4j/core/DiscordClientBuilder.java
@@ -38,7 +38,10 @@ import discord4j.rest.RestClient;
 import discord4j.rest.http.ExchangeStrategies;
 import discord4j.rest.http.client.DiscordWebClient;
 import discord4j.rest.request.DefaultRouterFactory;
+import discord4j.rest.request.Router;
 import discord4j.rest.request.RouterFactory;
+import discord4j.rest.request.RouterOptions;
+import discord4j.rest.response.ResponseFunction;
 import discord4j.store.api.service.StoreService;
 import discord4j.store.api.service.StoreServiceLoader;
 import discord4j.store.api.util.StoreContext;
@@ -94,6 +97,9 @@ public final class DiscordClientBuilder {
 
     @Nullable
     private RouterFactory routerFactory;
+
+    @Nullable
+    private RouterOptions routerOptions;
 
     @Nullable
     private GatewayClientFactory gatewayClientFactory;
@@ -299,6 +305,16 @@ public final class DiscordClientBuilder {
     }
 
     /**
+     * Return the current {@link RouterOptions} used to configure {@link RouterFactory} instances.
+     *
+     * @return the current {@code RouterOptions} used by this client
+     */
+    @Nullable
+    public RouterOptions getRouterOptions() {
+        return routerOptions;
+    }
+
+    /**
      * Set a new {@link RouterFactory} used to create a {@link discord4j.rest.request.Router} that executes Discord
      * REST API requests.
      * <p>
@@ -313,6 +329,24 @@ public final class DiscordClientBuilder {
      */
     public DiscordClientBuilder setRouterFactory(@Nullable RouterFactory routerFactory) {
         this.routerFactory = routerFactory;
+        return this;
+    }
+
+    /**
+     * Sets a new {@link RouterOptions} used to configure a {@link RouterFactory}.
+     * <p>
+     * {@code RouterOptions} instances provide a way to override the {@link Scheduler} used for retrieving API responses
+     * and scheduling rate limiting actions. It also allows changing the behavior associated with API errors through
+     * {@link RouterOptions.Builder#onClientResponse(ResponseFunction)}.
+     * <p>
+     * If you use a default {@code RouterFactory}, it will use the supplied {@code RouterOptions} to configure itself
+     * while building this client.
+     *
+     * @param routerOptions a new {@code RouterOptions} to configure a {@code RouterFactory}
+     * @return this builder
+     */
+    public DiscordClientBuilder setRouterOptions(@Nullable RouterOptions routerOptions) {
+        this.routerOptions = routerOptions;
         return this;
     }
 
@@ -557,6 +591,13 @@ public final class DiscordClientBuilder {
         return new DefaultRouterFactory();
     }
 
+    private Router initRouter(RouterFactory factory, DiscordWebClient webClient) {
+        if (routerOptions != null) {
+            return factory.getRouter(webClient, routerOptions);
+        }
+        return factory.getRouter(webClient);
+    }
+
     private GatewayClientFactory initGatewayClientFactory() {
         if (gatewayClientFactory != null) {
             return gatewayClientFactory;
@@ -599,7 +640,7 @@ public final class DiscordClientBuilder {
         final HttpClient httpClient = initHttpClient();
         final DiscordWebClient webClient = initWebClient(httpClient, jackson.getObjectMapper());
         final RouterFactory routerFactory = initRouterFactory();
-        final RestClient restClient = new RestClient(routerFactory.getRouter(webClient));
+        final RestClient restClient = new RestClient(initRouter(routerFactory, webClient));
 
         // Prepare Stores
         final StoreService storeService = initStoreService();

core/src/test/java/discord4j/core/ExampleBot.java
@@ -27,6 +27,10 @@ import discord4j.core.object.reaction.ReactionEmoji;
 import discord4j.core.object.util.Image;
 import discord4j.core.object.util.Snowflake;
 import discord4j.rest.http.client.ClientException;
+import discord4j.rest.request.RouteMatcher;
+import discord4j.rest.request.RouterOptions;
+import discord4j.rest.response.ResponseFunction;
+import discord4j.rest.route.Routes;
 import org.apache.commons.io.IOUtils;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
@@ -36,16 +40,17 @@ import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
+import reactor.retry.Retry;
 import reactor.util.Logger;
 import reactor.util.Loggers;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class ExampleBot {
@@ -64,7 +69,20 @@ public class ExampleBot {
     @Test
     @Ignore("Example code excluded from CI")
     public void testCommandBot() {
-        DiscordClient client = new DiscordClientBuilder(token).build();
+        DiscordClient client = new DiscordClientBuilder(token)
+                .setRouterOptions(RouterOptions.builder()
+                        // globally suppress any not found (404) error
+                        .onClientResponse(ResponseFunction.emptyIfNotFound())
+                        // wait 1 second and retry any server error (500)
+                        .onClientResponse(ResponseFunction.retryOnceOnErrorStatus(500))
+                        // bad requests (400) while adding reactions will be suppressed
+                        .onClientResponse(ResponseFunction.emptyOnErrorStatus(RouteMatcher.route(Routes.REACTION_CREATE), 400))
+                        // server error (500) while creating a message will be retried, with backoff, until it succeeds
+                        .onClientResponse(ResponseFunction.retryWhen(RouteMatcher.route(Routes.MESSAGE_CREATE),
+                                Retry.onlyIf(ClientException.isRetryContextStatusCode(500))
+                                        .exponentialBackoffWithJitter(Duration.ofSeconds(2), Duration.ofSeconds(10))))
+                        .build())
+                .build();
 
         // Get the bot owner ID to filter commands
         AtomicLong ownerId = new AtomicLong();
@@ -151,6 +169,10 @@ public class ExampleBot {
                                                 .addField("Avatar URL", user.getAvatarUrl(), false)
                                                 .setImage(user.getAvatarUrl()))
                                 )))
+                        .switchIfEmpty(Mono.just("Not found")
+                                .flatMap(reason -> message.getChannel()
+                                        .flatMap(channel -> channel.createMessage(reason)))
+                        )
                         .then();
             }
             return Mono.empty();
@@ -258,7 +280,7 @@ public class ExampleBot {
                         }
                         return Flux.fromIterable(fetch(count))
                                 .flatMap(emoji -> message.addReaction(ReactionEmoji.unicode(emoji))
-                                        .onErrorContinue(isStatusCode(400),
+                                        .onErrorContinue(ClientException.isStatusCode(400),
                                                 (t, o) -> log.info("Dropping {} due to {}", t.toString()))
                                 )
                                 .then();
@@ -309,15 +331,5 @@ public class ExampleBot {
     public static abstract class EventHandler {
 
         public abstract Mono<Void> onMessageCreate(MessageCreateEvent event);
-    }
-
-    private static Predicate<Throwable> isStatusCode(int code) {
-        return t -> {
-            if (t instanceof ClientException) {
-                ClientException e = (ClientException) t;
-                return e.getStatus().code() == code;
-            }
-            return false;
-        };
     }
 }

core/src/test/resources/logback.xml
@@ -2,7 +2,7 @@
 <configuration scan="true">
     <logger name="io.netty" level="INFO"/>
     <logger name="reactor" level="INFO"/>
-    <logger name="reactor.retry" level="DEBUG"/>
+    <logger name="reactor.retry" level="INFO"/>
     <logger name="discord4j.core" level="INFO"/>
     <logger name="discord4j.gateway.client" level="INFO"/>
     <logger name="discord4j.gateway.inbound" level="INFO"/>

rest/src/main/java/discord4j/rest/http/client/ClientException.java
@@ -18,12 +18,56 @@
 package discord4j.rest.http.client;
 
 import discord4j.rest.json.response.ErrorResponse;
+import discord4j.rest.response.ResponseFunction;
+import discord4j.rest.route.Route;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClientResponse;
+import reactor.retry.Backoff;
+import reactor.retry.Retry;
+import reactor.retry.RetryContext;
 import reactor.util.annotation.Nullable;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * Exception that contains information about a failed request containing HTTP response data.
+ * <p>
+ * The original request can be retrieved through {@link #getRequest()}. HTTP response status can be retrieved by
+ * {@link #getStatus()}, headers using {@link #getHeaders()}, while the body can be retrieved through
+ * {@link #getErrorResponse()} provided Discord has supplied a body along with the error.
+ * <p>
+ * It is possible to modify the behavior of a reactive sequence that has failed with this error, using operators like
+ * {@link Mono#onErrorResume(Predicate, Function)}, {@link Mono#onErrorContinue(Predicate, BiConsumer)} among others. In
+ * cases where a {@link Predicate} is accepted, you can use one of the provided static methods like
+ * {@link #isStatusCode(int)}
+ * to further filter by HTTP status code.
+ * <p>
+ * The following example would retry a request if it has failed with an HTTP 500 error:
+ * <pre>
+ * client.getEventDispatcher().on(MessageCreateEvent.class)
+ *     .map(MessageCreateEvent::getMessage)
+ *     .filter(msg -&gt; msg.getContent().map("!ping"::equals).orElse(false))
+ *     .flatMap(Message::getChannel)
+ *     .flatMap(channel -&gt; channel.createMessage("Pong!")
+ *         .transform(ClientException.retryOnceOnStatus(500)))
+ *     .subscribe();
+ * </pre>
+ * While the following one would transform a not found user into an empty sequence:
+ * <pre>
+ * client.getUserById(Snowflake.of(userLongId))
+ *     .onErrorResume(ClientException.isStatusCode(404), error -&gt; Mono.empty())
+ *     .subscribe(user -&gt; System.out.println("Found: " + user.getUsername()));
+ * </pre>
+ * For global or {@link Route} based error handling, refer to the {@link ResponseFunction} class.
+ */
 public class ClientException extends RuntimeException {
 
     private final ClientRequest request;
@@ -31,8 +75,15 @@ public class ClientException extends RuntimeException {
     private final HttpHeaders headers;
     private final ErrorResponse errorResponse;
 
+    /**
+     * Create a new {@link ClientException} with the given HTTP request and response details.
+     *
+     * @param request the original {@link ClientRequest} that caused this exception
+     * @param response the failing {@link HttpClientResponse}
+     * @param errorResponse the response body converted to an {@link ErrorResponse}, or {@code null} if not available
+     */
     public ClientException(ClientRequest request, HttpClientResponse response, @Nullable ErrorResponse errorResponse) {
-        super(request.method().toString() + " " + request.url() + " returned " + response.status().toString() +
+        super(request.getMethod().toString() + " " + request.getUrl() + " returned " + response.status().toString() +
                 (errorResponse != null ? " with response " + errorResponse.getFields() : ""));
         this.request = request;
         this.status = response.status();
@@ -40,18 +91,42 @@ public class ClientException extends RuntimeException {
         this.errorResponse = errorResponse;
     }
 
+    /**
+     * Return the {@link ClientRequest} encapsulating a Discord API request.
+     *
+     * @return the request that caused this exception
+     */
     public ClientRequest getRequest() {
         return request;
     }
 
+    /**
+     * Return the {@link HttpResponseStatus} with information related to the HTTP error. The actual status code can be
+     * obtained through {@link HttpResponseStatus#code()}.
+     *
+     * @return the HTTP error associated to this exception
+     */
     public HttpResponseStatus getStatus() {
         return status;
     }
 
+    /**
+     * Return the {@link HttpHeaders} from the error <strong>response</strong>. To get request headers refer to
+     * {@link #getRequest()} and then {@link ClientRequest#getHeaders()}.
+     *
+     * @return the HTTP response headers
+     */
     public HttpHeaders getHeaders() {
         return headers;
     }
 
+    /**
+     * Return the HTTP response body in the form of a Discord {@link ErrorResponse}, if present. {@link ErrorResponse}
+     * is a common object that contains an internal status code and messages, and could be used to further clarify
+     * the source of the API error.
+     *
+     * @return the Discord error response, if present.
+     */
     @Nullable
     public ErrorResponse getErrorResponse() {
         return errorResponse;
@@ -65,5 +140,95 @@ public class ClientException extends RuntimeException {
                 ", headers=" + headers.copy().remove(HttpHeaderNames.AUTHORIZATION).toString() +
                 ", errorResponse=" + errorResponse +
                 "}";
+    }
+
+    /**
+     * {@link Predicate} helper to further classify a {@link ClientException} depending on the underlying HTTP status
+     * code.
+     *
+     * @param code the status code for which this {@link Predicate} should return {@code true}
+     * @return a {@link Predicate} that returns {@code true} if the given {@link Throwable} is a {@link ClientException}
+     * containing the given HTTP status code.
+     */
+    public static Predicate<Throwable> isStatusCode(int code) {
+        return t -> {
+            if (t instanceof ClientException) {
+                ClientException e = (ClientException) t;
+                return e.getStatus().code() == code;
+            }
+            return false;
+        };
+    }
+
+    /**
+     * {@link Predicate} helper to further classify a {@link ClientException} depending on the underlying HTTP status
+     * code.
+     *
+     * @param codes the status codes for which this {@link Predicate} should return {@code true}
+     * @return a {@link Predicate} that returns {@code true} if the given {@link Throwable} is a {@link ClientException}
+     * containing the given HTTP status code.
+     */
+    public static Predicate<Throwable> isStatusCode(Integer... codes) {
+        return t -> {
+            if (t instanceof ClientException) {
+                ClientException e = (ClientException) t;
+                return Arrays.asList(codes).contains(e.getStatus().code());
+            }
+            return false;
+        };
+    }
+
+    /**
+     * {@link Predicate} helper to further classify a {@link ClientException}, while creating a {@link Retry}
+     * factory, depending on the underlying HTTP status code. A {@link Retry} factory can be created through methods
+     * like {@link Retry#onlyIf(Predicate)} where this method can be used as argument.
+     *
+     * @param code the status code for which this {@link Predicate} should return {@code true}
+     * @return a {@link Predicate} that returns {@code true} if the given {@link RetryContext} exception is a
+     * {@link ClientException} containing the given HTTP status code.
+     */
+    public static Predicate<RetryContext<?>> isRetryContextStatusCode(int code) {
+        return ctx -> isStatusCode(code).test(ctx.exception());
+    }
+
+    /**
+     * {@link Predicate} helper to further classify a {@link ClientException}, while creating a {@link Retry}
+     * factory, depending on the underlying HTTP status code. A {@link Retry} factory can be created through methods
+     * like {@link Retry#onlyIf(Predicate)} where this method can be used as argument.
+     *
+     * @param codes the status codes for which this {@link Predicate} should return {@code true}
+     * @return a {@link Predicate} that returns {@code true} if the given {@link Throwable} is a {@link ClientException}
+     * containing the given HTTP status code.
+     */
+    public static Predicate<RetryContext<?>> isRetryContextStatusCode(Integer... codes) {
+        return ctx -> isStatusCode(codes).test(ctx.exception());
+    }
+
+    /**
+     * Transformation function that can be used within an operator such as {@link Mono#transform(Function)} or
+     * {@link Mono#compose(Function)} to turn an error sequence matching the given HTTP status code, into an empty
+     * sequence, effectively suppressing the original error.
+     *
+     * @param code the status code that should be transformed into empty sequences
+     * @param <T> the type of the response
+     * @return a transformation function that converts error sequences into empty sequences.
+     */
+    public static <T> Function<Mono<T>, Publisher<T>> emptyOnStatus(int code) {
+        return mono -> mono.onErrorResume(isStatusCode(code), t -> Mono.empty());
+    }
+
+    /**
+     * Transformation function that can be used within an operator such as {@link Mono#transform(Function)} or
+     * {@link Mono#compose(Function)} to apply a retrying strategy in case of an error matching the given HTTP status
+     * code. The provided retrying strategy will wait 1 second, and then retry once.
+     *
+     * @param code the status code that should be retried
+     * @param <T> the type of the response
+     * @return a transformation function that retries error sequences.
+     */
+    public static <T> Function<Mono<T>, Publisher<T>> retryOnceOnStatus(int code) {
+        return mono -> mono.retryWhen(Retry.onlyIf(isRetryContextStatusCode(code))
+                .backoff(Backoff.fixed(Duration.ofSeconds(1)))
+                .retryOnce());
     }
 }

rest/src/main/java/discord4j/rest/http/client/ClientRequest.java
@@ -17,41 +17,92 @@
 
 package discord4j.rest.http.client;
 
+import discord4j.rest.request.DiscordRequest;
+import discord4j.rest.route.Route;
+import discord4j.rest.util.RouteUtils;
+import io.netty.handler.codec.http.DefaultHttpHeaders;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpMethod;
 
+import java.util.Optional;
+
 /**
- * A wrapper over a partial HTTP client request definition.
+ * An adapted request definition from an original {@link DiscordRequest}.
  */
 public class ClientRequest {
 
-    private final HttpMethod method;
+    private final DiscordRequest<?> request;
     private final String url;
     private final HttpHeaders headers;
 
-    public ClientRequest(HttpMethod method, String url, HttpHeaders headers) {
-        this.method = method;
-        this.url = url;
-        this.headers = headers;
+    /**
+     * Create a new {@link ClientRequest} from the given request template.
+     *
+     * @param request the {@link DiscordRequest} template
+     */
+    public ClientRequest(DiscordRequest<?> request) {
+        this.request = request;
+        this.url = RouteUtils.expandQuery(request.getCompleteUri(), request.getQueryParams());
+        this.headers = Optional.ofNullable(request.getHeaders())
+                .map(map -> map.entrySet().stream()
+                        .reduce((HttpHeaders) new DefaultHttpHeaders(), (headers, entry) -> {
+                            String key = entry.getKey();
+                            entry.getValue().forEach(value -> headers.add(key, value));
+                            return headers;
+                        }, HttpHeaders::add))
+                .orElse(new DefaultHttpHeaders());
     }
 
-    public HttpMethod method() {
-        return method;
+    /**
+     * Return the HTTP method.
+     *
+     * @return the {@link HttpMethod} of this {@link ClientRequest}
+     */
+    public HttpMethod getMethod() {
+        return request.getRoute().getMethod();
     }
 
-    public String url() {
+    /**
+     * Return the request URL.
+     *
+     * @return the request URL for this {@link ClientRequest}
+     */
+    public String getUrl() {
         return url;
     }
 
-    public HttpHeaders headers() {
+    /**
+     * Return the headers of this request.
+     *
+     * @return the {@link HttpHeaders} of this {@link ClientRequest}
+     */
+    public HttpHeaders getHeaders() {
         return headers;
+    }
+
+    /**
+     * Return the original request template.
+     *
+     * @return the {@link DiscordRequest} template that created this {@link ClientRequest}
+     */
+    public DiscordRequest<?> getDiscordRequest() {
+        return request;
+    }
+
+    /**
+     * Return the API endpoint targeted by this request.
+     *
+     * @return the {@link Route} requested by this {@link ClientRequest}
+     */
+    public Route<?> getRoute() {
+        return request.getRoute();
     }
 
     @Override
     public String toString() {
         return "ClientRequest{" +
-                "method=" + method +
+                "method=" + getMethod() +
                 ", url='" + url + '\'' +
                 ", headers=" + headers.copy().remove(HttpHeaderNames.AUTHORIZATION).toString() +
                 '}';

commit 379219cd7cf88c9799f4200f53b83ffb50f9c90d

Add backpressure to request queue
Fix a regression introduced in 635af28 that made ordering requests
in the same bucket impossible while under operators such as
flatMap or fire-and-forget subscribe calls.

rest/src/main/java/discord4j/rest/request/RequestStream.java
@@ -19,6 +19,7 @@ package discord4j.rest.request;
 import discord4j.rest.http.client.ClientException;
 import discord4j.rest.http.client.ClientRequest;
 import discord4j.rest.http.client.DiscordWebClient;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.*;
 import reactor.core.scheduler.Scheduler;
 import reactor.netty.http.client.HttpClientResponse;
@@ -162,6 +163,11 @@ class RequestStream<T> {
         }
 
         @Override
+        protected void hookOnSubscribe(Subscription subscription) {
+            request(1);
+        }
+
+        @Override
         protected void hookOnNext(RequestCorrelation<T> correlation) {
             DiscordRequest<T> request = correlation.getRequest();
             MonoProcessor<T> callback = correlation.getResponse();
@@ -210,6 +216,7 @@ class RequestStream<T> {
                     logger.trace("Ready to consume next request after {}", signal);
                 }
                 sleepTime = Duration.ZERO;
+                request(1);
             }, t -> logger.error("Error while scheduling next request", t));
         }
 