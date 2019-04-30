453fe0a19ecea112722f7a4bde653fa0b51c8a93
xaanit <shadowjacob@yahoo.com>
2019-04-29 20:47:21 -0700

    Fix NoSuchElementException on Mono<Void> returns

--- core/src/main/kotlin/discord4j/core/Extension.kt
+++ core/src/main/kotlin/discord4j/core/Extension.kt
@@ -13,6 +13,6 @@ internal suspend fun <T> Mono<T>.await(): T = awaitSingle()
 internal suspend fun <T> Flux<T>.await(): List<T> = collectList().await()
 internal fun <T> Flux<T>.infinite(): ReceiveChannel<T> = openSubscription()
 internal fun <T> Optional<T>.grab(): T? = orElse(null)
-internal suspend fun Mono<Void>.unit(): Unit = await().let {}
+internal suspend fun Mono<Void>.unit(): Unit = awaitNull().let {}
 internal fun OptionalInt.grab(): Int? = takeIf { isPresent }?.asInt
 internal fun OptionalLong.grab(): Long? = takeIf { isPresent }?.asLong
