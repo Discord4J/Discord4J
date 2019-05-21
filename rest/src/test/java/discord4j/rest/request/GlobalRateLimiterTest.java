33bf52b3f3300a092bf819c59b32adc996ca21a4
quanticc <quanticc@users.noreply.github.com>
2019-05-21 18:26:06 -0400

    Update tests

--- rest/src/test/java/discord4j/rest/request/GlobalRateLimiterTest.java
+++ rest/src/test/java/discord4j/rest/request/GlobalRateLimiterTest.java
@@ -8,50 +8,48 @@
  *
  * Discord4J is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Discord4J.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Discord4J. If not, see <http://www.gnu.org/licenses/>.
  */
 package discord4j.rest.request;
 
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.Logger;
+import reactor.util.Loggers;
 
 import java.time.Duration;
 import java.util.Random;
 
 public class GlobalRateLimiterTest {
 
+    private static final Logger log = Loggers.getLogger(GlobalRateLimiterTest.class);
+
     @Test
     public void testGlobalRateLimiter() {
-        GlobalRateLimiter rateLimiter = new GlobalRateLimiter(14);
-
+        GlobalRateLimiter rateLimiter = new SemaphoreGlobalRateLimiter(8);
         rateLimiter.rateLimitFor(Duration.ofSeconds(1));
-
-        rateLimiter.onComplete().block();
-        System.out.println("1");
-
+        rateLimiter.withLimiter(Mono.just("1").doOnNext(log::info)).blockLast();
         rateLimiter.rateLimitFor(Duration.ofSeconds(1));
-
-        rateLimiter.onComplete().block();
-        System.out.println("2");
+        rateLimiter.withLimiter(Mono.just("2").doOnNext(log::info)).blockLast();
     }
 
     @Test
     public void testBurstingRequestsGlobalRateLimiter() {
-        GlobalRateLimiter rateLimiter = new GlobalRateLimiter(14);
+        GlobalRateLimiter rateLimiter = new SemaphoreGlobalRateLimiter(8);
         Random random = new Random();
         Flux.range(0, 100)
-                .flatMap(index -> rateLimiter.withLimiter(() -> {
+                .flatMap(index -> rateLimiter.withLimiter(Mono.defer(() -> {
                     if (random.nextDouble() < 0.1) {
                         long delay = random.nextInt(500);
                         rateLimiter.rateLimitFor(Duration.ofMillis(delay));
                     }
                     return Mono.just(index);
-                }))
+                })))
                 .blockLast();
     }
 }
