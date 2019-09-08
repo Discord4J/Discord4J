6648e937c961fc6f1ae519f6645520b22d7bc663
David <bi-crosseur@hotmail.fr>
2019-09-08 14:48:39 +0200

    Fix indentation

--- core/src/main/java/discord4j/core/object/entity/Member.java
+++ core/src/main/java/discord4j/core/object/entity/Member.java
@@ -74,8 +74,8 @@ public final class Member extends User {
     @Override
     public Mono<Member> asMember(final Snowflake guildId) {
         return Mono.just(this)
-            .filter(member -> member.getGuildId().equals(guildId))
-            .switchIfEmpty(super.asMember(guildId));
+                .filter(member -> member.getGuildId().equals(guildId))
+                .switchIfEmpty(super.asMember(guildId));
     }
 
     /**
