b7943e88787d01887085cb73e8fa2334493195f9
Shadorc <bi-crosseur@hotmail.fr>
2020-01-03 12:55:35 +0100

    Why Intellij removed this

--- core/src/main/java/discord4j/core/object/ExtendedInvite.java
+++ core/src/main/java/discord4j/core/object/ExtendedInvite.java
@@ -18,6 +18,9 @@ package discord4j.core.object;
 
 import discord4j.core.ServiceMediator;
 import discord4j.core.object.data.ExtendedInviteBean;
+import discord4j.core.object.entity.User;
+import discord4j.core.object.util.Snowflake;
+import reactor.core.publisher.Mono;
 
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
