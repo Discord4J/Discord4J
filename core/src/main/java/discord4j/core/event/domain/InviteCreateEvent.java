728a5323220216b71b229b8792aa4bf8c26057c5
Pedro Arenas <nachito94@msn.com>
2020-01-18 16:19:04 -0300

    Update core/src/main/java/discord4j/core/event/domain/InviteCreateEvent.java
    Co-Authored-By: David <bi-crosseur@hotmail.fr>

--- core/src/main/java/discord4j/core/event/domain/InviteCreateEvent.java
+++ core/src/main/java/discord4j/core/event/domain/InviteCreateEvent.java
@@ -118,7 +118,7 @@ public class InviteCreateEvent extends Event {
             ", maxUses=" + maxUses +
             ", maxAge=" + maxAge +
             ", temporary=" + temporary +
-            ", createdAt='" + createdAt +
+            ", createdAt='" + createdAt + '\'' +
             '}';
     }
 }
