80ed812896cd2a140a4a82cf945e02c614555882
David <bi-crosseur@hotmail.fr>
2020-10-15 23:31:26 +0200

    Update RestGuild

--- core/src/main/java/discord4j/core/object/entity/Webhook.java
+++ core/src/main/java/discord4j/core/object/entity/Webhook.java
@@ -184,6 +184,15 @@ public final class Webhook implements Entity {
     }
 
     /**
+     * Gets the bot/OAuth2 application ID that created this webhook.
+     *
+     * @return The bot/OAuth2 application ID that created this webhook.
+     */
+    public Optional<Snowflake> getApplicationId() {
+        return data.applicationId().map(Snowflake::of);
+    }
+
+    /**
      * Requests to delete this webhook.
      *
      * @return A {@link Mono} where, upon successful completion, emits nothing; indicating the webhook has been deleted.
