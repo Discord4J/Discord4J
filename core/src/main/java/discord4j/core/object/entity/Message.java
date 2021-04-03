da8ae506a51b5d42bda656118f929de754ad75a3
David <bi-crosseur@hotmail.fr>
2021-04-03 03:15:52 +0200

    Add loading message flag (#883)

--- core/src/main/java/discord4j/core/object/entity/Message.java
+++ core/src/main/java/discord4j/core/object/entity/Message.java
@@ -649,19 +649,13 @@ public final class Message implements Entity {
      */
     public enum Flag {
 
-        /**
-         * This message has been published to subscribed channels (via Channel Following).
-         */
+        /** This message has been published to subscribed channels (via Channel Following). */
         CROSSPOSTED(0),
 
-        /**
-         * This message originated from a message in another channel (via Channel Following).
-         */
+        /** This message originated from a message in another channel (via Channel Following). */
         IS_CROSSPOST(1),
 
-        /**
-         * Do not include any embeds when serializing this message.
-         */
+        /** Do not include any embeds when serializing this message. */
         SUPPRESS_EMBEDS(2),
 
         /** The source message for this crosspost has been deleted (via Channel Following). */
@@ -671,7 +665,10 @@ public final class Message implements Entity {
         URGENT(4),
 
         /** This message is an ephemeral interaction response. */
-        EPHEMERAL(6);
+        EPHEMERAL(6),
+
+        /** This message is an Interaction Response and the bot is "thinking". */
+        LOADING(7);
 
         /**
          * The underlying value as represented by Discord.
