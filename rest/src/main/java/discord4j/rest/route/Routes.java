40fb11e865ffd0c24089a3a0499d70e869c905c7
David <bi-crosseur@hotmail.fr>
2021-04-11 06:17:23 +0200

    Deprecate UserService#getUserDMs for removal (#890)

--- rest/src/main/java/discord4j/rest/route/Routes.java
+++ rest/src/main/java/discord4j/rest/route/Routes.java
@@ -911,6 +911,7 @@ public abstract class Routes {
      * @see <a href="https://discord.com/developers/docs/resources/user#get-user-dms">
      * https://discord.com/developers/docs/resources/user#get-user-dms</a>
      */
+    @Deprecated
     public static final Route USER_DMS_GET = Route.get("/users/@me/channels");
 
     /**
