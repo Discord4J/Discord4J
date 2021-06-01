a35dd3677e1d2104c63e9010951b3786acb16d10
oopsjpeg <oopsjpeg@gmail.com>
2021-06-01 18:52:39 -0400

    Add ApplicationCommandInteractEvent and ButtonInteractEvent

--- core/src/main/java/discord4j/core/event/domain/interaction/InteractionCreateEvent.java
+++ core/src/main/java/discord4j/core/event/domain/interaction/InteractionCreateEvent.java
@@ -1,25 +1,9 @@
-/*
- * This file is part of Discord4J.
- *
- * Discord4J is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Discord4J is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Discord4J. If not, see <http://www.gnu.org/licenses/>.
- */
+package discord4j.core.event.domain.interaction;
 
-package discord4j.core.event.domain;
-
-import discord4j.common.annotations.Experimental;
 import discord4j.common.util.Snowflake;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.Event;
+import discord4j.core.object.command.ApplicationCommandInteraction;
 import discord4j.core.object.command.Interaction;
 import discord4j.core.object.component.MessageComponent;
 import discord4j.core.object.entity.Message;
@@ -35,16 +19,6 @@ import reactor.core.publisher.Mono;
 import java.util.Optional;
 import java.util.function.Consumer;
 
-// TODO: update docs
-// TODO: maybe do some subtyping here? ButtonInteractionEvent, SlashCommandEvent. Not 1:1, but much nicer
-/**
- * Dispatched when a user in a guild uses a Slash Command.
- * <p>
- * This event is dispatched by Discord.
- *
- * @see <a href="https://discord.com/developers/docs/topics/gateway#interaction-create">Interaction Create</a>
- */
-@Experimental
 public class InteractionCreateEvent extends Event {
 
     private final Interaction interaction;
@@ -65,42 +39,16 @@ public class InteractionCreateEvent extends Event {
         return interaction;
     }
 
-    private ApplicationCommandInteractionData getCommandInteractionData() {
-        return interaction.getData().data().get();
-    }
-
-    /**
-     * Gets the id of the invoked command.
-     *
-     * @return The id of the invoked command.
-     */
-    public Optional<Snowflake> getCommandId() {
-        return getCommandInteractionData().id().toOptional().map(Snowflake::of);
-    }
-
-    /**
-     * Gets the name of the invoked command.
-     *
-     * @return The name of the invoked command.
-     */
-    public Optional<String> getCommandName() {
-        return getCommandInteractionData().name().toOptional();
-    }
-
     public Optional<String> getCustomId() {
-        return getCommandInteractionData().customId().toOptional();
+        return getCommandInteraction().flatMap(ApplicationCommandInteraction::getCustomId);
+    }
+
+    public Optional<ApplicationCommandInteraction> getCommandInteraction() {
+        return interaction.getCommandInteraction();
     }
 
     public Optional<MessageComponent.Type> getComponentType() {
-        return getCommandInteractionData().componentType().toOptional().map(MessageComponent.Type::of);
-    }
-
-    private Mono<Void> createInteractionResponse(InteractionResponseData responseData) {
-        long id = interaction.getId().asLong();
-        String token = interaction.getToken();
-
-        return getClient().rest().getInteractionService()
-                .createInteractionResponse(id, token, responseData);
+        return getCommandInteraction().flatMap(ApplicationCommandInteraction::getComponentType);
     }
 
     /**
@@ -117,6 +65,14 @@ public class InteractionCreateEvent extends Event {
                 .type(InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE.getValue())
                 .data(InteractionApplicationCommandCallbackData.builder().build())
                 .build());
+    }
+
+    private Mono<Void> createInteractionResponse(InteractionResponseData responseData) {
+        long id = interaction.getId().asLong();
+        String token = interaction.getToken();
+
+        return getClient().rest().getInteractionService()
+                .createInteractionResponse(id, token, responseData);
     }
 
     /**
@@ -149,22 +105,6 @@ public class InteractionCreateEvent extends Event {
     }
 
     /**
-     * Requests to respond to the interaction with only
-     * {@link InteractionApplicationCommandCallbackSpec#setContent(String) content} and
-     * {@link InteractionApplicationCommandCallbackSpec#setEphemeral(boolean) ephemeral set to true}. Only the invoking
-     * user can see it.
-     *
-     * @param content A string message to populate the message with.
-     * @return A {@link Mono} where, upon successful completion, emits nothing; indicating the ephemeral interaction
-     * response has been sent. If an error is received, it is emitted through the {@code Mono}.
-     * @see InteractionApplicationCommandCallbackSpec#setContent(String)
-     * @see InteractionApplicationCommandCallbackSpec#setEphemeral(boolean)
-     */
-    public Mono<Void> replyEphemeral(final String content) {
-        return reply(spec -> spec.setContent(content).setEphemeral(true));
-    }
-
-    /**
      * Requests to respond to the interaction with a message.
      *
      * @param spec A {@link Consumer} that provides a "blank" {@link InteractionApplicationCommandCallbackSpec} to be
@@ -186,6 +126,22 @@ public class InteractionCreateEvent extends Event {
                             .data(mutatedSpec.asRequest())
                             .build());
                 });
+    }
+
+    /**
+     * Requests to respond to the interaction with only
+     * {@link InteractionApplicationCommandCallbackSpec#setContent(String) content} and
+     * {@link InteractionApplicationCommandCallbackSpec#setEphemeral(boolean) ephemeral set to true}. Only the invoking
+     * user can see it.
+     *
+     * @param content A string message to populate the message with.
+     * @return A {@link Mono} where, upon successful completion, emits nothing; indicating the ephemeral interaction
+     * response has been sent. If an error is received, it is emitted through the {@code Mono}.
+     * @see InteractionApplicationCommandCallbackSpec#setContent(String)
+     * @see InteractionApplicationCommandCallbackSpec#setEphemeral(boolean)
+     */
+    public Mono<Void> replyEphemeral(final String content) {
+        return reply(spec -> spec.setContent(content).setEphemeral(true));
     }
 
     /**
