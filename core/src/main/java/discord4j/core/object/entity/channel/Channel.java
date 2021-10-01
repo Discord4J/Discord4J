6b731b04076197dc9151f60c55784199d90a5e59
NovaFox161 <novamaday@gmail.com>
2021-10-01 11:34:52 -0500

    Add missing switch branches to Channel.Type.of

--- core/src/main/java/discord4j/core/object/entity/channel/Channel.java
+++ core/src/main/java/discord4j/core/object/entity/channel/Channel.java
@@ -159,6 +159,9 @@ public interface Channel extends Entity {
                 case 4: return GUILD_CATEGORY;
                 case 5: return GUILD_NEWS;
                 case 6: return GUILD_STORE;
+                case 10: return GUILD_NEWS_THREAD;
+                case 11: return GUILD_PUBLIC_THREAD;
+                case 12: return GUILD_PRIVATE_THREAD;
                 case 13: return GUILD_STAGE_VOICE;
                 default: return UNKNOWN;
             }
