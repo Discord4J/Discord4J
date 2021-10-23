2569d327cabdb9394ed4d5e1d35006f428f9096a
Skat <55407440+skykatik@users.noreply.github.com>
2021-10-23 21:12:14 +0400

    Add #getData to PermissionOverwrite object (#1024)

--- core/src/main/java/discord4j/core/spec/legacy/LegacyTextChannelEditSpec.java
+++ core/src/main/java/discord4j/core/spec/legacy/LegacyTextChannelEditSpec.java
@@ -94,12 +94,7 @@ public class LegacyTextChannelEditSpec implements LegacyAuditSpec<ChannelModifyR
      */
     public LegacyTextChannelEditSpec setPermissionOverwrites(Set<? extends PermissionOverwrite> permissionOverwrites) {
         List<OverwriteData> raw = permissionOverwrites.stream()
-                .map(o -> OverwriteData.builder()
-                        .id(o.getTargetId().asString())
-                        .type(o.getType().getValue())
-                        .allow(o.getAllowed().getRawValue())
-                        .deny(o.getDenied().getRawValue())
-                        .build())
+                .map(PermissionOverwrite::getData)
                 .collect(Collectors.toList());
 
         requestBuilder.permissionOverwrites(raw);
