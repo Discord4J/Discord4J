a23a2ca189f09aba19be4bc3f48f681f9efb0609
Doc <nachito94@msn.com>
2022-01-25 11:44:09 -0300

    Add support for get the Sticker url

--- core/src/main/java/discord4j/core/object/entity/PartialSticker.java
+++ core/src/main/java/discord4j/core/object/entity/PartialSticker.java
@@ -2,10 +2,18 @@ package discord4j.core.object.entity;
 
 import discord4j.common.util.Snowflake;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.util.ImageUtil;
 import discord4j.discordjson.json.PartialStickerData;
+
 import java.util.Objects;
 
+import static discord4j.rest.util.Image.Format.LOTTIE;
+import static discord4j.rest.util.Image.Format.PNG;
+
 public class PartialSticker implements Entity {
+
+    /** The path for {@code PartialSticker} image URLs. */
+    private static final String STICKER_IMAGE_PATH = "stickers/%s";
 
     /**
      * The gateway associated to this object.
@@ -48,6 +56,16 @@ public class PartialSticker implements Entity {
      */
     public Sticker.Format getFormatType() {
         return Sticker.Format.of(data.formatType());
+    }
+
+    /**
+     * Gets the URL for this sticker.
+     *
+     * @return The URL for this sticker.
+     */
+    public String getImageUrl() {
+        final String path = String.format(STICKER_IMAGE_PATH, getId().asString());
+        return getFormatType().equals(Sticker.Format.LOTTIE) ? ImageUtil.getUrl(path, LOTTIE) : ImageUtil.getUrl(path, PNG);
     }
 
     /**
