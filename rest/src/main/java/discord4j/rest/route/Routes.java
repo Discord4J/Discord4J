c94d894d26f5a3c97a6a0230157926ea996f7cde
Doc <nachito94@msn.com>
2022-05-20 18:24:24 -0400

    [ci skip] [Draft] Add a Route for AutoMod things (disabled for bots)

--- rest/src/main/java/discord4j/rest/route/Routes.java
+++ rest/src/main/java/discord4j/rest/route/Routes.java
@@ -73,6 +73,18 @@ public abstract class Routes {
     public static final Route AUDIT_LOG_GET = Route.get("/guilds/{guild.id}/audit-logs");
 
     //////////////////////////////////////////////
+    ////////////// AutoMod Resource //////////////
+    //////////////////////////////////////////////
+
+    /**
+     * Returns an auto mod rule object for the guild. Requires the 'MANAGE_GUILD' permission.
+     *
+     * @see <a href="https://discord.com/developers/docs/resources/list-auto-moderation-rules-for-guild">
+     * https://discord.com/developers/docs/resources/list-auto-moderation-rules-for-guild</a>
+     */
+    public static final Route AUTO_MOD_RULES_GET = Route.get("/guilds/{guild.id}/auto-moderation/rules/");
+
+    //////////////////////////////////////////////
     ////////////// Channel Resource //////////////
     //////////////////////////////////////////////
 
