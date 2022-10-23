b13f2bd1b6a5dad7940e2ccc0235e9dbfe73b952
Florian Morgan <florian.morgan@gmail.com>
2022-10-23 17:50:25 +0200

    Fix Permission enum according to official discord doc (#1100)

--- rest/src/main/java/discord4j/rest/util/Permission.java
+++ rest/src/main/java/discord4j/rest/util/Permission.java
@@ -85,9 +85,6 @@ public enum Permission {
     /** Allows the usage of custom emojis from other servers. */
     USE_EXTERNAL_EMOJIS(0x00040000, false),
 
-    /** Allows the usage of custom stickers from other servers. */
-    USE_EXTERNAL_STICKERS(0x00000020000000, false),
-
     /** Allows for viewing guild insights. */
     VIEW_GUILD_INSIGHTS(0x00080000, false),
 
@@ -132,14 +129,42 @@ public enum Permission {
     /** Allows management and editing of emojis and stickers. */
     MANAGE_EMOJIS_AND_STICKERS(0x40000000, true),
 
-    /** Allows members to use slash commands in text channels. */
+    /** Allows members to use slash commands in text channels.
+     *
+     *  @deprecated Deprecated in favor of {@link Permission#USE_APPLICATION_COMMANDS}
+     */
+    @Deprecated
     USE_SLASH_COMMANDS(0x80000000L, false),
+
+    /** Allows members to use application commands, including slash commands and context menu commands. */
+    USE_APPLICATION_COMMANDS(0x80000000L, false),
 
     /**
      * Allows for requesting to speak in stage channels.
      */
     @Experimental
-    REQUEST_TO_SPEAK(0x100000000L, false),
+    REQUEST_TO_SPEAK(0x0000000100000000L, false),
+
+    /** Allows for creating, editing, and deleting scheduled events. */
+    MANAGE_EVENTS(0x0000000200000000L, false),
+
+    /** Allows for deleting and archiving threads, and viewing all private threads. */
+    MANAGE_THREADS(0x0000000400000000L, true),
+
+    /** Allows for creating public and announcement threads. */
+    CREATE_PUBLIC_THREADS(0x0000000800000000L, false),
+
+    /** Allows for creating private threads. */
+    CREATE_PRIVATE_THREADS(0x0000001000000000L, false),
+
+    /** Allows the usage of custom stickers from other servers. */
+    USE_EXTERNAL_STICKERS(0x0000002000000000L, false),
+
+    /** Allows for sending messages in threads */
+    SEND_MESSAGES_IN_THREADS(0x0000004000000000L, false),
+
+    /** Allows for using Activities (applications with the EMBEDDED flag) in a voice channel. */
+    USE_EMBEDDED_ACTIVITIES(0x0000008000000000L, false),
 
     /**
      * Allows for timing out users to prevent them from sending or reacting to messages in chat and threads, and from speaking in voice and stage channels.
