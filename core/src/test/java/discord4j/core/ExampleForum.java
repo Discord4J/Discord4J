5b0f28aebd09aefc95e740c12c33afd5a330cc65
quanticc <abarcab.ivan@gmail.com>
2023-06-05 01:26:10 -0400

    Add parent_id env var to forum channel example

--- core/src/test/java/discord4j/core/ExampleForum.java
+++ core/src/test/java/discord4j/core/ExampleForum.java
@@ -18,39 +18,21 @@
 package discord4j.core;
 
 import discord4j.common.util.Snowflake;
-import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
-import discord4j.core.event.domain.interaction.MessageInteractionEvent;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
-import discord4j.core.event.domain.thread.ThreadEvent;
-import discord4j.core.object.command.ApplicationCommand;
-import discord4j.core.object.command.ApplicationCommandInteractionOption;
-import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
-import discord4j.core.object.command.ApplicationCommandOption;
 import discord4j.core.object.component.ActionRow;
 import discord4j.core.object.component.Button;
 import discord4j.core.object.entity.ForumTag;
-import discord4j.core.object.entity.User;
 import discord4j.core.object.entity.channel.ForumChannel;
-import discord4j.core.object.entity.channel.ThreadChannel;
-import discord4j.core.object.entity.channel.TopLevelGuildMessageChannel;
 import discord4j.core.spec.*;
-import discord4j.discordjson.json.ApplicationCommandOptionData;
-import discord4j.discordjson.json.ApplicationCommandRequest;
-import discord4j.rest.interaction.GuildCommandRegistrar;
+import discord4j.discordjson.possible.Possible;
 import discord4j.rest.util.Color;
-import org.reactivestreams.Publisher;
-import reactor.core.publisher.Mono;
-import reactor.util.Logger;
-import reactor.util.Loggers;
 
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
-
-import static reactor.function.TupleUtils.function;
+import java.util.Optional;
 
 /**
  * Showcase some forum channels operations under a given test guild. Requires TOKEN and GUILD_ID environment variables.
+ * <p>
+ * You can optionally include a PARENT_ID category to create the forum channel inside a category.
  */
 public class ExampleForum {
 
@@ -60,47 +42,54 @@ public class ExampleForum {
     public static void main(String[] args) {
         // Those are tags that will be created in the forum channel
         final ForumTagCreateSpec testTag1 = ForumTagCreateSpec.builder()
-            .name("Tag 1")
-            .emojiNameOrNull("✅")
-            .build(),
-            testTag2 = ForumTagCreateSpec.builder()
-                .name("Tag 2")
-                .emojiNameOrNull("❤️")
-                .build();
+                .name("Tag 1")
+                .emojiNameOrNull("✅")
+                .build(),
+                testTag2 = ForumTagCreateSpec.builder()
+                        .name("Tag 2")
+                        .emojiNameOrNull("❤️")
+                        .build();
+
+        Possible<Snowflake> parentId = Optional.ofNullable(System.getenv("PARENT_ID"))
+                .map(Long::parseLong)
+                .map(it -> Possible.of(Snowflake.of(it)))
+                .orElse(Possible.absent());
 
         DiscordClient.create(TOKEN)
-            .withGateway(client -> client.on(ReadyEvent.class, event -> client.getGuildById(Snowflake.of(GUILD_ID))
-                //Create forum channel
-                .flatMap(guild -> guild.createForumChannel(ForumChannelCreateSpec.builder()
-                    .name("test-forum")
-                    .addAvailableTags(testTag1, testTag2)
-                    .defaultSortOrderOrNull(ForumChannel.SortOrder.LATEST_ACTIVITY.getValue())
-                    .defaultForumLayoutOrNull(ForumChannel.LayoutType.GALLERY_VIEW.getValue())
-                    .reason("Creating the test forum channel")
-                    .build()))
-                //Starting a thread
-                .flatMap(channel -> {
-                    Snowflake tag2Id = channel.getAvailableTags().stream()
-                        .filter(tag -> tag.getName().equals("Tag 2"))
-                        .findFirst()
-                        .map(ForumTag::getId)
-                        .orElseThrow(IllegalArgumentException::new);
+                .withGateway(client -> client.on(ReadyEvent.class, event -> client.getGuildById(Snowflake.of(GUILD_ID))
+                        //Create forum channel
+                        .flatMap(guild -> guild.createForumChannel(ForumChannelCreateSpec.builder()
+                                .name("test-forum")
+                                .parentId(parentId)
+                                .addAvailableTags(testTag1, testTag2)
+                                .defaultSortOrderOrNull(ForumChannel.SortOrder.LATEST_ACTIVITY.getValue())
+                                .defaultForumLayoutOrNull(ForumChannel.LayoutType.GALLERY_VIEW.getValue())
+                                .reason("Creating the test forum channel")
+                                .build()))
+                        //Starting a thread
+                        .flatMap(channel -> {
+                            Snowflake tag2Id = channel.getAvailableTags().stream()
+                                    .filter(tag -> tag.getName().equals("Tag 2"))
+                                    .findFirst()
+                                    .map(ForumTag::getId)
+                                    .orElseThrow(IllegalArgumentException::new);
 
-                    return channel.startThread(StartThreadInForumChannelSpec.builder()
-                        .name("Test Thread")
-                        .addAppliedTag(tag2Id)
-                        .reason("Creating a test thread")
-                        .message(ForumThreadMessageCreateSpec.builder()
-                            .content("Message content")
-                            .addComponent(ActionRow.of(Button.primary("test", "Test button (does nothing)")))
-                            .addEmbed(EmbedCreateSpec.builder()
-                                .title("Test embed")
-                                .color(Color.ORANGE)
-                                .build())
-                            .build())
-                        .build());
-                })
-            ))
-            .block();
+                            return channel.startThread(StartThreadInForumChannelSpec.builder()
+                                    .name("Test Thread")
+                                    .addAppliedTag(tag2Id)
+                                    .reason("Creating a test thread")
+                                    .message(ForumThreadMessageCreateSpec.builder()
+                                            .content("Message content")
+                                            .addComponent(ActionRow.of(Button.primary("test",
+                                                    "Test button (does nothing)")))
+                                            .addEmbed(EmbedCreateSpec.builder()
+                                                    .title("Test embed")
+                                                    .color(Color.ORANGE)
+                                                    .build())
+                                            .build())
+                                    .build());
+                        })
+                ))
+                .block();
     }
 }
