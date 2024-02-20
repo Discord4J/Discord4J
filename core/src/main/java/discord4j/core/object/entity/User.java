854653e2284412f0dc2daf4e7fbc4ebc351d3a38
EasternGamer <crysishunter32@gmail.com>
2024-02-20 14:02:13 +0200

    Update BOM for Reactor Added .equal override for PartialMember.java to account for guild ID. Made .equal override in User.java no longer final to enable override in PartialMember.java

--- core/src/main/java/discord4j/core/object/entity/User.java
+++ core/src/main/java/discord4j/core/object/entity/User.java
@@ -341,7 +341,7 @@ public class User implements Entity {
     }
 
     @Override
-    public final boolean equals(@Nullable final Object obj) {
+    public boolean equals(@Nullable final Object obj) {
         return EntityUtil.equals(this, obj);
     }
 
