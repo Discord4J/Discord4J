9885a62d50c6d19b5c7532b31d946447ec146b6c
Doc <nachito94@msn.com>
2024-07-22 16:45:13 -0400

    Route to new method for emoji id in Guild

--- core/src/main/java/discord4j/core/object/entity/Guild.java
+++ core/src/main/java/discord4j/core/object/entity/Guild.java
@@ -530,8 +530,21 @@ public final class Guild implements Entity {
      * @return A {@link Mono} where, upon successful completion, emits the {@link GuildEmoji} as represented by the
      * supplied ID. If an error is received, it is emitted through the {@code Mono}.
      */
-    public Mono<GuildEmoji> getGuildEmojiById(final Snowflake id) {
+    public Mono<GuildEmoji> getEmojiById(final Snowflake id) {
         return gateway.getGuildEmojiById(getId(), id);
+    }
+
+    /**
+     * Requests to retrieve the guild emoji as represented by the supplied ID.
+     *
+     * @param id The ID of the guild emoji.
+     * @return A {@link Mono} where, upon successful completion, emits the {@link GuildEmoji} as represented by the
+     * supplied ID. If an error is received, it is emitted through the {@code Mono}.
+     * @deprecated Deprecated in favor of {@link #getEmojiById(Snowflake)}
+     */
+    @Deprecated
+    public Mono<GuildEmoji> getGuildEmojiById(final Snowflake id) {
+        return this.getEmojiById(id);
     }
 
     /**
@@ -542,8 +555,22 @@ public final class Guild implements Entity {
      * @return A {@link Mono} where, upon successful completion, emits the {@link GuildEmoji} as represented by the
      * supplied ID. If an error is received, it is emitted through the {@code Mono}.
      */
-    public Mono<GuildEmoji> getGuildEmojiById(final Snowflake id, EntityRetrievalStrategy retrievalStrategy) {
+    public Mono<GuildEmoji> getEmojiById(final Snowflake id, EntityRetrievalStrategy retrievalStrategy) {
         return gateway.withRetrievalStrategy(retrievalStrategy).getGuildEmojiById(getId(), id);
+    }
+
+    /**
+     * Requests to retrieve the guild emoji as represented by the supplied ID, using the given retrieval strategy.
+     *
+     * @param id The ID of the guild emoji.
+     * @param retrievalStrategy the strategy to use to get the guild emoji
+     * @return A {@link Mono} where, upon successful completion, emits the {@link GuildEmoji} as represented by the
+     * supplied ID. If an error is received, it is emitted through the {@code Mono}.
+     * @deprecated Deprecated in favor of {@link #getEmojiById(Snowflake, EntityRetrievalStrategy)}
+     */
+    @Deprecated
+    public Mono<GuildEmoji> getGuildEmojiById(final Snowflake id, EntityRetrievalStrategy retrievalStrategy) {
+        return this.getEmojiById(id, retrievalStrategy);
     }
 
     /**
