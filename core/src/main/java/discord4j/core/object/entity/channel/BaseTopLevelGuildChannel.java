274126561f3015903f571c0e7a6fb540f03cd643
Axel Joly <azn9.pro@gmail.com>
2024-08-19 21:25:06 +0200

    fix: fix #1269

--- core/src/main/java/discord4j/core/object/entity/channel/BaseTopLevelGuildChannel.java
+++ core/src/main/java/discord4j/core/object/entity/channel/BaseTopLevelGuildChannel.java
@@ -29,7 +29,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-class BaseTopLevelGuildChannel extends BaseChannel implements TopLevelGuildChannel {
+public class BaseTopLevelGuildChannel extends BaseChannel implements TopLevelGuildChannel {
 
     BaseTopLevelGuildChannel(GatewayDiscordClient gateway, ChannelData data) {
         super(gateway, data);
