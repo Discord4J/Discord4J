746068e560b6a20d5e011f5acab465d9196bc52a
Doc <nachito94@msn.com>
2024-10-19 10:19:48 -0300

    Improvement in PartialMessage - Add methods based in the available fields for forwarded messages

--- core/src/main/java/discord4j/core/object/PartialMessage.java
+++ core/src/main/java/discord4j/core/object/PartialMessage.java
@@ -1,10 +1,28 @@
 package discord4j.core.object;
 
+import discord4j.common.annotations.Experimental;
+import discord4j.common.util.Snowflake;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.object.component.LayoutComponent;
+import discord4j.core.object.component.MessageComponent;
+import discord4j.core.object.entity.Attachment;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.PartialMember;
+import discord4j.core.object.entity.PartialSticker;
+import discord4j.core.object.entity.User;
 import discord4j.discordjson.json.PartialMessageData;
 
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 /**
- * A Partial Message used by the Forward Message feature.
+ * A Partial Message where just a few elements from a {@link Message} are present.
+ * <b>Note:</b> this is just a partial representation of elements where many of them can not be present and not fully represent the full data of the message
  *
  * @see <a href="https://discord.com/developers/docs/resources/message#message-snapshot-object">
  * Message Snapshot Object</a>
@@ -29,6 +47,157 @@ public class PartialMessage implements DiscordObject {
 
     public PartialMessageData getData() {
         return this.data;
+    }
+
+    /**
+     * Gets the type of message.
+     *
+     * @return The type of message.
+     */
+    public Optional<Message.Type> getType() {
+        return data.type().toOptional().map(Message.Type::of);
+    }
+
+    /**
+     * Returns the flags of this {@link PartialMessage} if are present, describing its features.
+     *
+     * @return A {@code EnumSet} with the flags of this message.
+     */
+    public EnumSet<Message.Flag> getFlags() {
+        return data.flags().toOptional()
+            .map(Message.Flag::of)
+            .orElse(EnumSet.noneOf(Message.Flag.class));
+    }
+
+    /**
+     * Gets the ID of the guild this message is associated to, if this {@code PartialMessage} was built from Gateway data,
+     * like an incoming event. If requested from REST API, this field will be empty.
+     *
+     * @return The ID of the guild this message is associated to, if present.
+     */
+    public Optional<Snowflake> getGuildId() {
+        return data.guildId().toOptional().map(Snowflake::of);
+    }
+
+    /**
+     * Gets the author of this message, if present.
+     *
+     * @return The author of this message, if present.
+     */
+    public Optional<User> getAuthor() {
+        return (!data.author().isAbsent() && (data.webhookId().isAbsent() || !data.interaction().isAbsent())) ?
+            Optional.of(new User(gateway, data.author().get())) : Optional.empty();
+    }
+
+    /**
+     * Gets the partial members specifically mentioned in this message, without duplication and with the same order
+     * as in the message.
+     *
+     * @return The partial members specifically mentioned in this message, without duplication and with the same order
+     * as in the message.
+     */
+    public List<PartialMember> getMemberMentions() {
+        if (data.guildId().isAbsent()) {
+            return Collections.emptyList();
+        }
+        long guildId = data.guildId().get().asLong();
+        return data.mentions().stream()
+            .map(data -> new PartialMember(gateway, data, data.member().get(), guildId))
+            .collect(Collectors.toList());
+    }
+
+    /**
+     * Gets the IDs of the roles specifically mentioned in this message, without duplication and with the same order
+     * as in the message.
+     *
+     * @return The IDs of the roles specifically mentioned in this message, without duplication and with the same order
+     * as in the message.
+     */
+    public List<Snowflake> getRoleMentionIds() {
+        return data.mentionRoles().stream()
+            .map(Snowflake::of)
+            .collect(Collectors.toList());
+    }
+
+    /**
+     * Gets the contents of the message, if present.
+     *
+     * @return The contents of the message, if present.
+     */
+    public Optional<String> getContent() {
+        return data.content().toOptional();
+    }
+
+    /**
+     * Gets any embedded content.
+     *
+     * @return Any embedded content.
+     */
+    public List<Embed> getEmbeds() {
+        return data.embeds().stream()
+            .map(data1 -> new Embed(gateway, data1))
+            .collect(Collectors.toList());
+    }
+
+    /**
+     * Gets any attached files, with the same order as in the message.
+     *
+     * @return Any attached files, with the same order as in the message.
+     */
+    public List<Attachment> getAttachments() {
+        return data.attachments().stream()
+            .map(data -> new Attachment(gateway, data))
+            .collect(Collectors.toList());
+    }
+
+    /**
+     * Gets the partial stickers sent with the message.
+     *
+     * @return The partial stickers sent with the message.
+     */
+    @Experimental
+    public List<PartialSticker> getStickersItems() {
+        return data.stickerItems().toOptional()
+            .map(partialStickers -> partialStickers.stream()
+                .map(data -> new PartialSticker(gateway, data))
+                .collect(Collectors.toList()))
+            .orElse(Collections.emptyList());
+    }
+
+    /**
+     * Gets the components on the message.
+     *
+     * @return The components on the message.
+     */
+    public List<LayoutComponent> getComponents() {
+        return data.components().toOptional()
+            .map(componentList -> componentList.stream()
+                .map(MessageComponent::fromData)
+                // top level message components should only be LayoutComponents
+                .filter(component -> component instanceof LayoutComponent)
+                .map(component -> (LayoutComponent) component)
+                .collect(Collectors.toList()))
+            .orElse(Collections.emptyList());
+    }
+
+    /**
+     * Gets when this message was sent.
+     *
+     * @return When this message was sent.
+     */
+    public Optional<Instant> getTimestamp() {
+        return data.timestamp().toOptional()
+            .map(timestamp -> DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse(timestamp, Instant::from));
+    }
+
+    /**
+     * Gets when this message was edited, if present.
+     *
+     * @return When this message was edited, if present.
+     */
+    public Optional<Instant> getEditedTimestamp() {
+        return data.editedTimestamp()
+            .map(timestamp -> DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse(timestamp, Instant::from));
     }
 
 
