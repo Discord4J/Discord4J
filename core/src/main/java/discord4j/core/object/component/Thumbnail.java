73cc11a655d57ed7cdcceebac92a625ba937896f
Doc <nachito94@msn.com>
2025-02-20 10:35:19 -0300

    Thumbnail docs

--- core/src/main/java/discord4j/core/object/component/Thumbnail.java
+++ core/src/main/java/discord4j/core/object/component/Thumbnail.java
@@ -21,16 +21,42 @@ import discord4j.discordjson.possible.Possible;
 
 import java.util.Optional;
 
+/**
+ * A thumbnail component for message.
+ *
+ * @see <a href="https://discord.com/developers/docs/interactions/message-components#???">Thumbnail</a>
+ */
 public class Thumbnail extends MessageComponent {
 
+    /**
+     * Creates an {@code Thumbnail}.
+     *
+     * @param media The media
+     * @return An {@code Thumbnail}
+     */
     public static Thumbnail of(UnfurledMediaItem media) {
         return new Thumbnail(MessageComponent.getBuilder(Type.THUMBNAIL).media(media.getData()).build());
     }
 
+    /**
+     * Creates an {@code Thumbnail}.
+     *
+     * @param media The media
+     * @param description The description
+     * @return An {@code Thumbnail}
+     */
     public static Thumbnail of(UnfurledMediaItem media, String description) {
         return new Thumbnail(MessageComponent.getBuilder(Type.THUMBNAIL).media(media.getData()).description(Possible.of(Optional.of(description))).build());
     }
 
+    /**
+     * Creates an {@code Thumbnail}.
+     *
+     * @param media The media
+     * @param description The description
+     * @param spoiler If this component is a spoiler
+     * @return An {@code Thumbnail}
+     */
     public static Thumbnail of(UnfurledMediaItem media, String description, boolean spoiler) {
         return new Thumbnail(MessageComponent.getBuilder(Type.THUMBNAIL).media(media.getData()).description(Possible.of(Optional.of(description))).spoiler(spoiler).build());
     }
@@ -39,10 +65,20 @@ public class Thumbnail extends MessageComponent {
         super(data);
     }
 
+    /**
+     * Gets the media related to this thumbnail.
+     *
+     * @return An {@code UnfurledMediaItem}
+     */
     public UnfurledMediaItem getMedia() {
         return new UnfurledMediaItem(this.getData().media().get());
     }
 
+    /**
+     * Gets the description for this thumbnail.
+     *
+     * @return The description
+     */
     public String getDescription() {
         return Possible.flatOpt(this.getData().description()).orElse("");
     }
