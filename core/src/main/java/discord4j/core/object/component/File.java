d2060aa054e235edb305696eb326c84869251f20
Doc <nachito94@msn.com>
2025-02-20 10:02:28 -0300

    add File docs

--- core/src/main/java/discord4j/core/object/component/File.java
+++ core/src/main/java/discord4j/core/object/component/File.java
@@ -18,12 +18,30 @@ package discord4j.core.object.component;
 
 import discord4j.discordjson.json.ComponentData;
 
+/**
+ * A file component for message.
+ *
+ * @see <a href="https://discord.com/developers/docs/interactions/message-components#???">File</a>
+ */
 public class File extends MessageComponent {
 
+    /**
+     * Creates an {@code File} with the given {@code UnfurledMediaItem}.
+     *
+     * @param file The file component with an {@code attachment://} reference
+     * @return An {@code SectionComponent} containing the given components
+     */
     public static File of(UnfurledMediaItem file) {
         return new File(MessageComponent.getBuilder(Type.FILE).file(file.getData()).build());
     }
 
+    /**
+     * Creates an {@code File} with the given {@code UnfurledMediaItem}.
+     *
+     * @param file The file component with an {@code attachment://} reference
+     * @param spoiler Sets the spoiler
+     * @return An {@code SectionComponent} containing the given components
+     */
     public static File of(UnfurledMediaItem file, boolean spoiler) {
         return new File(MessageComponent.getBuilder(Type.FILE).file(file.getData()).spoiler(spoiler).build());
     }
@@ -32,10 +50,20 @@ public class File extends MessageComponent {
         super(data);
     }
 
+    /**
+     * Gets the media file attached to this component.
+     *
+     * @return a {@link UnfurledMediaItem}
+     */
     public UnfurledMediaItem getFile() {
         return new UnfurledMediaItem(this.getData().file().get());
     }
 
+    /**
+     * Gets if this component is a spoiler.
+     *
+     * @return {@code true} if is spoiler, false otherwise
+     */
     public boolean isSpoiler() {
         return this.getData().spoiler().toOptional().orElse(false);
     }
