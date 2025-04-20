e87bbcde5b4bdc173d0862641d3cdec5d3172d81
Doc <nachito94@msn.com>
2025-04-20 10:23:52 -0400

    Add modify rest method for SoundboardService

--- rest/src/main/java/discord4j/rest/service/SoundboardService.java
+++ rest/src/main/java/discord4j/rest/service/SoundboardService.java
@@ -20,10 +20,12 @@ package discord4j.rest.service;
 import discord4j.discordjson.json.SendSoundboardSoundRequest;
 import discord4j.discordjson.json.SoundboardSoundData;
 import discord4j.discordjson.json.SoundboardSoundDataList;
+import discord4j.discordjson.json.SoundboardSoundModifyRequest;
 import discord4j.rest.request.Router;
 import discord4j.rest.route.Routes;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.annotation.Nullable;
 
 public class SoundboardService extends RestService {
 
@@ -55,6 +57,14 @@ public class SoundboardService extends RestService {
 
     public Mono<SoundboardSoundData> getGuildSoundboardSound(long guildId, long soundBoardId) {
         return Routes.GET_GUILD_SOUNDBOARD_SOUND.newRequest(guildId, soundBoardId)
+            .exchange(getRouter())
+            .bodyToMono(SoundboardSoundData.class);
+    }
+
+    public Mono<SoundboardSoundData> modifyGuildSoundboardSound(long guildId, long soundBoardId, SoundboardSoundModifyRequest request, @Nullable String reason) {
+        return Routes.MODIFY_GUILD_SOUNDBOARD_SOUND.newRequest(guildId, soundBoardId)
+            .body(request)
+            .optionalHeader("X-Audit-Log-Reason", reason)
             .exchange(getRouter())
             .bodyToMono(SoundboardSoundData.class);
     }
