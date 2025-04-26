d6181d193dbafef7662dcc6e0b07efe401112932
Doc <nachito94@msn.com>
2025-04-26 10:22:42 -0400

    [ci skip] fix style SoundboardService

--- rest/src/main/java/discord4j/rest/service/SoundboardService.java
+++ rest/src/main/java/discord4j/rest/service/SoundboardService.java
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with Discord4J.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package discord4j.rest.service;
 
 import discord4j.discordjson.json.SendSoundboardSoundRequest;
