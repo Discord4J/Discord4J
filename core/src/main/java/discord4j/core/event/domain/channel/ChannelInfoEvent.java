4cce6da5952d9f856d778799da4009cd8139daf5
Doc <git@mrdoc.dev>
2026-07-20 18:18:21 -0400

    feat: add events

---
+++ core/src/main/java/discord4j/core/event/domain/channel/ChannelInfoEvent.java
@@ -0,0 +1,62 @@
+/*
+ * This file is part of Discord4J.
+ *
+ * Discord4J is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Discord4J is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Discord4J. If not, see <http://www.gnu.org/licenses/>.
+ */
+package discord4j.core.event.domain.channel;
+
+import discord4j.common.util.Snowflake;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.object.entity.Guild;
+import discord4j.gateway.ShardInfo;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+/**
+ * Dispatched when a Request Channel Info is requested.
+ * <p>
+ * This event is dispatched by Discord.
+ *
+ * @see <a href="https://docs.discord.com/developers/events/gateway-events#channel-info">
+ * Channel Info</a>
+ */
+public class ChannelInfoEvent extends ChannelEvent {
+
+    private final long guildId;
+    private final List<? extends Object> channels;
+
+    public ChannelInfoEvent(
+            final GatewayDiscordClient gateway,
+            final ShardInfo shardInfo,
+            long guildId,
+            List<? extends Object> channels
+    ) {
+        super(gateway, shardInfo);
+        this.guildId = guildId;
+        this.channels = channels;
+    }
+
+    /**
+     *
+     */
+    public Snowflake getGuildId() {
+        return Snowflake.of(this.guildId);
+    }
+
+    public Mono<Guild> getGuild() {
+        return this.getClient().getGuildById(this.getGuildId());
+    }
+
+}
